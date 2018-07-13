a89bae42ba746e232e0f5bd030817282952eb2e3
Bruno Schmitt <bruno.schmitt@epfl.ch>
2018-07-13 22:02:15 +0200

    Let there be a world; Hello World!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![License](https://img.shields.io/badge/license-MIT-000000.svg)](https://opensource.org/licenses/MIT)
+
 In the beginning there was only darkness.
---
+++ examples/hello_world.cpp
@@ -0,0 +1,13 @@
+/*------------------------------------------------------------------------------
+| This file is distributed under the MIT License.
+| See accompanying file /LICENSE for details.
+| Author(s): Bruno Schmitt < bruno [at] oschmitt [dot] com >
+*-----------------------------------------------------------------------------*/
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	(void) argc;
+	(void) argv;
+	std::cout << "Hello world!\n";
+}
