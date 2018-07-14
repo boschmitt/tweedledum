ac9371bc141b47327e94873c95194f0f25e6e24b
Bruno Schmitt <bruno.schmitt@epfl.ch>
2018-07-14 10:32:49 +0200

    Fix and change ``dumdee`` example.

--- examples/dumdee.cpp
+++ examples/dumdee.cpp
@@ -6,6 +6,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <tweedledum/io/dotqc.hpp>
+#include <tweedledum/io/write_dot.hpp>
+#include <tweedledum/io/write_qpic.hpp>
 #include <tweedledum/representations/quantum_circuit.hpp>
 
 using namespace tweedledum;
@@ -16,8 +18,10 @@ int main(int argc, char** argv)
 		std::cerr << "[e] Input file not specified.\n";
 		return EXIT_FAILURE;
 	}
-	quantum_circuit qc;
-	dotqc_reader<quantum_circuit> reader(qc);
+	quantum_circuit<dag_path<gate>> qc_path;
+	dotqc_reader<quantum_circuit<dag_path<gate>>> reader(qc_path);
 	dotqc_read(argv[1], reader, identify_gate_kind());
+	write_dot(qc_path, "qc_dag_path.dot");
+	write_qpic(qc_path, "qc_dat_path.qpic");
 	return EXIT_SUCCESS;
 }
