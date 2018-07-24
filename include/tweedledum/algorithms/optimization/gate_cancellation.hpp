cfee494a868d8bd8c3515c54d41a2850051025db
Bruno Schmitt <bruno.schmitt@epfl.ch>
2018-07-24 13:38:25 +0200

    Fix: when looking for possible cancelations among controlled gates, it is important to make sure there is no gate between the controls that influence the result.

--- include/tweedledum/algorithms/optimization/gate_cancellation.hpp
+++ include/tweedledum/algorithms/optimization/gate_cancellation.hpp
@@ -49,18 +49,37 @@ void controlled_gate_cancellation(Network& net, bool remove_gates = true)
 		if (node.gate.is_controlled() == false) {
 			return;
 		}
+		typename Network::node_ptr_type index_to_remove;
+		auto do_remove = false;
 		auto children = net.get_children(node, node.gate.target());
 		for (auto child : children) {
 			auto& child_node = net.get_node(child);
-			if (net.mark(child_node) || node.gate.is_controlled() == false) {
+			if (net.mark(child_node) || child_node.gate.kind() != node.gate.kind()) {
 				continue;
 			}
 			if (node == child_node) {
-				net.mark(node, 1);
-				net.mark(child_node, 1);
-				to_delete += 2;
-				return;
+				index_to_remove = child;
+				do_remove = true;
+				break;
 			}
+		}
+		if (do_remove == false) {
+			return;
+		}
+		node.gate.foreach_control([&](auto& qubit) {
+			auto control_children = net.get_children(node, qubit);
+			for (auto child : control_children) {
+				if (child == index_to_remove) {
+					return;
+				}
+			}
+			do_remove = false;
+		});
+		if (do_remove) {
+			auto& node_to_remove = net.get_node(index_to_remove);
+			net.mark(node, 1);
+			net.mark(node_to_remove, 1);
+			to_delete += 2;
 		}
 	});
 	if (remove_gates) {
