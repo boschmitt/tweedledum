8d7042725bc5351fdaf72ebc1f3af50a774a3354
Bruno Schmitt <bruno.schmitt@epfl.ch>
2018-09-28 11:35:28 +0200

    Cirq format writer: small fixes, style changes, adding documentation, adding simple test.

--- include/tweedledum/io/write_cirq.hpp
+++ include/tweedledum/io/write_cirq.hpp
@@ -17,8 +17,24 @@
 
 namespace tweedledum {
 
+/*! \brief Writes network in CirQ format into output stream
+ *
+ * An overloaded variant exists that writes the network into a file.
+ *
+ * **Required gate functions:**
+ * - `kind`
+ * - `foreach_control`
+ * - `foreach_target`
+ *
+ * **Required network functions:**
+ * - `num_qubits`
+ * - `foreach_node`
+ *
+ * \param network Network
+ * \param os Output stream
+ */
 template<typename Network>
-void write_cirq(Network const& network, std::ostream& out)
+void write_cirq(Network const& network, std::ostream& os)
 {
 	network.foreach_node([&](auto const& n) {
 		auto const& g = n.gate;
@@ -34,41 +50,41 @@ void write_cirq(Network const& network, std::ostream& out)
 
 		case gate_kinds_t::hadamard:
 			g.foreach_target(
-			    [&](auto q) { out << fmt::format("qc.append(cirq.H(qs[{}]))\n", q); });
+			    [&](auto q) { os << fmt::format("qc.append(cirq.H(qs[{}]))\n", q); });
 			break;
 
 		case gate_kinds_t::pauli_x:
 			g.foreach_target(
-			    [&](auto q) { out << fmt::format("qc.append(cirq.X(qs[{}]))\n", q); });
+			    [&](auto q) { os << fmt::format("qc.append(cirq.X(qs[{}]))\n", q); });
 			break;
 
 		case gate_kinds_t::t:
 			g.foreach_target(
-			    [&](auto q) { out << fmt::format("qc.append(cirq.T(qs[{}]))\n", q); });
+			    [&](auto q) { os << fmt::format("qc.append(cirq.T(qs[{}]))\n", q); });
 			break;
 
 		case gate_kinds_t::t_dagger:
 			g.foreach_target([&](auto q) {
-				out << fmt::format("qc.append(cirq.RotZGate(rads=5."
-				                   "497787143782138)(qs[{}]))\n",
-				                   q);
+				os << fmt::format("qc.append(cirq.RotZGate(rads=5."
+				                  "497787143782138)(qs[{}]))\n",
+				                  q);
 			});
 			break;
 
 		case gate_kinds_t::rotation_z:
 			g.foreach_target([&](auto q) {
-				out << fmt::format("qc.append(cirq.RotZGate("
-				                   "rads={})(qs[{}]))\n",
-				                   g.angle(), q);
+				os << fmt::format("qc.append(cirq.RotZGate("
+				                  "rads={})(qs[{}]))\n",
+				                  g.rotation_angle(), q);
 			});
 			break;
 
 		case gate_kinds_t::cx:
 			g.foreach_control([&](auto qc) {
 				g.foreach_target([&](auto qt) {
-					out << fmt::format("qc.append(cirq.CNOT(qs[{}], "
-					                   "qs[{}]))\n",
-					                   qc, qt);
+					os << fmt::format("qc.append(cirq.CNOT(qs[{}], "
+					                  "qs[{}]))\n",
+					                  qc, qt);
 				});
 			});
 			break;
@@ -76,9 +92,9 @@ void write_cirq(Network const& network, std::ostream& out)
 		case gate_kinds_t::cz:
 			g.foreach_control([&](auto qc) {
 				g.foreach_target([&](auto qt) {
-					out << fmt::format("qc.append(cirq.CZ("
-					                   "qs[{}], qs[{}]))\n",
-					                   qc, qt);
+					os << fmt::format("qc.append(cirq.CZ("
+					                  "qs[{}], qs[{}]))\n",
+					                  qc, qt);
 				});
 			});
 			break;
@@ -93,29 +109,29 @@ void write_cirq(Network const& network, std::ostream& out)
 				return true;
 			case 0u:
 				for (auto q : targets) {
-					out << fmt::format("qc.append(cirq.X(qs[{}]))\n", q);
+					os << fmt::format("qc.append(cirq.X(qs[{}]))\n", q);
 				}
 				break;
 			case 1u:
 				for (auto q : targets) {
-					out << fmt::format("qc.append(cirq.CNOT(qs[{}], "
-					                   "qs[{}]))\n",
-					                   controls[0], q);
+					os << fmt::format("qc.append(cirq.CNOT(qs[{}], "
+					                  "qs[{}]))\n",
+					                  controls[0], q);
 				}
 				break;
 			case 2u:
 				for (auto i = 1u; i < targets.size(); ++i) {
-					out << fmt::format("qc.append(cirq.CNOT(qs[{}], "
-					                   "qs[{}]))\n",
-					                   targets[0], targets[i]);
+					os << fmt::format("qc.append(cirq.CNOT(qs[{}], "
+					                  "qs[{}]))\n",
+					                  targets[0], targets[i]);
 				}
-				out << fmt::format("qc.append(cirq.CCX(qs[{}], "
-				                   "qs[{}], qs[{}]))\n",
-				                   controls[0], controls[1], targets[0]);
+				os << fmt::format("qc.append(cirq.CCX(qs[{}], "
+				                  "qs[{}], qs[{}]))\n",
+				                  controls[0], controls[1], targets[0]);
 				for (auto i = 1u; i < targets.size(); ++i) {
-					out << fmt::format("qc.append(cirq.CNOT(qs[{}], "
-					                   "qs[{}]))\n",
-					                   targets[0], targets[i]);
+					os << fmt::format("qc.append(cirq.CNOT(qs[{}], "
+					                  "qs[{}]))\n",
+					                  targets[0], targets[i]);
 				}
 				break;
 			}
@@ -125,10 +141,24 @@ void write_cirq(Network const& network, std::ostream& out)
 	});
 }
 
+/*! \brief Writes network in CirQ format into a file
+ *
+ * **Required gate functions:**
+ * - `kind`
+ * - `foreach_control`
+ * - `foreach_target`
+ * 
+ * **Required network functions:**
+ * - `num_qubits`
+ * - `foreach_node`
+ *
+ * \param network Network
+ * \param filename Filename
+ */
 template<typename Network>
 void write_cirq(Network const& network, const std::string& filename)
 {
-	std::ofstream out(filename.c_str(), std::ofstream::out);
-	write_cirq(network, out);
+	std::ofstream os(filename.c_str(), std::ofstream::out);
+	write_cirq(network, os);
 }
 }; // namespace tweedledum
