e511a38e24c831a9e1214235fe2237ac0ad4452a
Mathias Soeken <mathias.soeken@epfl.ch>
2018-10-16 10:05:01 +0200

    Name esop_based_synthesis control_function_synthesis.

--- tests/algorithms/synthesis/single_target_gates.cpp
+++ tests/algorithms/synthesis/single_target_gates.cpp
@@ -14,27 +14,56 @@
 #include <tweedledum/networks/gg_network.hpp>
 #include <tweedledum/networks/netlist.hpp>
 
-TEST_CASE("Synthesize a simple function into a quantum network using stg_from_pprm", "[syn_stg]")
+using namespace tweedledum;
+
+namespace tweedledum::detail {
+
+template<class Network>
+inline std::pair<Network, std::vector<uint32_t>> circuit_and_map(uint32_t qubits)
 {
-	using namespace tweedledum;
-	kitty::dynamic_truth_table tt(5);
-	kitty::create_from_hex_string(tt, "DA657041");
-	// Looks bad :(
-	gg_network<mcmt_gate> gg_net = stg_from_pprm2<gg_network<mcmt_gate>>(tt);
+	Network circ;
+	for (auto i = 0u; i < qubits; ++i) {
+		circ.add_qubit();
+	}
+	std::vector<uint32_t> map(qubits);
+	std::iota(map.begin(), map.end(), 0u);
+	return {circ, map};
 }
 
-TEST_CASE("Synthesize a simple function into a quantum network using stg_from_pkrm", "[syn_stg]")
+} // namespace tweedledum::detail
+
+TEST_CASE("Synthesize a simple function into a quantum network using stg_from_pprm",
+          "[single_target_gates]")
 {
-	using namespace tweedledum;
 	kitty::dynamic_truth_table tt(5);
 	kitty::create_from_hex_string(tt, "DA657041");
-	gg_network<mcmt_gate> gg_net = stg_from_pkrm2<gg_network<mcmt_gate>>(tt);
+	auto [circ, map] = detail::circuit_and_map<gg_network<mcmt_gate>>(6u);
+	stg_from_pprm()(circ, tt, map);
+
+	CHECK(circ.num_gates() == 21u);
+	CHECK(circ.num_qubits() == 6u);
 }
 
-TEST_CASE("Synthesize a simple function into a quantum network using stg_from_spectrum", "[syn_stg]")
+TEST_CASE("Synthesize a simple function into a quantum network using stg_from_pkrm",
+          "[single_target_gates]")
 {
-	using namespace tweedledum;
 	kitty::dynamic_truth_table tt(5);
 	kitty::create_from_hex_string(tt, "DA657041");
-	gg_network<mcmt_gate> gg_net = stg_from_spectrum2<gg_network<mcmt_gate>>(tt);
+	auto [circ, map] = detail::circuit_and_map<gg_network<mcmt_gate>>(6u);
+	stg_from_pkrm()(circ, tt, map);
+
+	CHECK(circ.num_gates() == 38u);
+	CHECK(circ.num_qubits() == 6u);
+}
+
+TEST_CASE("Synthesize a simple function into a quantum network using stg_from_spectrum",
+          "[single_target_gates]")
+{
+	kitty::dynamic_truth_table tt(5);
+	kitty::create_from_hex_string(tt, "DA657041");
+	auto [circ, map] = detail::circuit_and_map<gg_network<mcmt_gate>>(6u);
+	stg_from_spectrum()(circ, tt, map);
+
+	CHECK(circ.num_gates() == 185u);
+	CHECK(circ.num_qubits() == 6u);
 }
