bc3e1908658e77737149664178306818f29ab6b5
Bruno Schmitt <bruno.schmitt@epfl.ch>
2018-11-26 19:33:24 +0100

    Defining an invalid qubit identifier.

---
+++ include/tweedledum/networks/qubit.hpp
@@ -0,0 +1,15 @@
+/*-------------------------------------------------------------------------------------------------
+| This file is distributed under the MIT License.
+| See accompanying file /LICENSE for details.
+| Author(s): Bruno Schmitt
+*------------------------------------------------------------------------------------------------*/
+#pragma once
+
+#include <cstdint>
+#include <limits>
+
+namespace tweedledum {
+
+constexpr static auto qid_invalid = std::numeric_limits<uint32_t>::max();
+
+} // namespace tweedledum
