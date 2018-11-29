9028902bac7af3298e4d584e1010edbb96dc2766
Bruno Schmitt <bruno.schmitt@epfl.ch>
2018-11-29 11:52:52 +0100

    'Linear synth' synthesis method

---
+++ tests/algorithms/synthesis/linear_synth.cpp
@@ -0,0 +1,33 @@
+/*-------------------------------------------------------------------------------------------------
+| This file is distributed under the MIT License.
+| See accompanying file /LICENSE for details.
+| Author(s): Bruno Schmitt
+*------------------------------------------------------------------------------------------------*/
+#include <catch.hpp>
+#include <tweedledum/algorithms/synthesis/linear_synth.hpp>
+#include <tweedledum/gates/angle.hpp>
+#include <tweedledum/gates/mcst_gate.hpp>
+#include <tweedledum/io/write_unicode.hpp>
+#include <tweedledum/networks/netlist.hpp>
+#include <tweedledum/utils/parity_terms.hpp>
+
+TEST_CASE("Synthesize a simple function into a quantum network using linear_synth",
+          "[linear_synth]")
+{
+	using namespace tweedledum;
+	parity_terms parities;
+	parities.add_term(3u, symbolic_angles::one_eighth);
+
+	SECTION("Using binary strategy")
+	{
+		auto network = linear_synth<netlist<mcst_gate>>(3u, parities, {linear_synth_params::strategy::binary});
+		write_unicode(network);
+	}
+
+	SECTION("Using gray strategy (default)")
+	{
+		auto network = linear_synth<netlist<mcst_gate>>(3u, parities);
+		write_unicode(network);
+	}
+
+}
