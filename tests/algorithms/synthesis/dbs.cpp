4203060dfce8b24ec419f51ac71ecb38289fbd56
Bruno Schmitt <bruno.schmitt@epfl.ch>
2018-11-29 16:05:02 +0100

    Small fix.

--- tests/algorithms/synthesis/dbs.cpp
+++ tests/algorithms/synthesis/dbs.cpp
@@ -25,7 +25,7 @@ TEST_CASE("Check DBS with PRIME(3) and PPRM", "[dbs]")
 	CHECK(network.num_qubits() == 3u);
 }
 
-TEST_CASE("Check DBS with PRIME(3) and spectrum", "[decomposition_based]")
+TEST_CASE("Check DBS with PRIME(3) and spectrum", "[dbs]")
 {
 	using namespace tweedledum;
 	std::vector<uint32_t> permutation{{0, 2, 3, 5, 7, 1, 4, 6}};
