106f074f077773685238c1aae07368f457d4b348
Bruno Schmitt <bruno.schmitt@epfl.ch>
2018-12-06 12:06:05 +0100

    Add missing file (barenco decomposition test).

---
+++ tests/algorithms/decomposition/barenco.cpp
@@ -0,0 +1,22 @@
+/*--------------------------------------------------------------------------------------------------
+| This file is distributed under the MIT License.
+| See accompanying file /LICENSE for details.
+| Author(s): Bruno Schmitt
+*-------------------------------------------------------------------------------------------------*/
+#include <catch.hpp>
+#include <tweedledum/algorithms/decomposition/barenco.hpp>
+#include <tweedledum/gates/mcmt_gate.hpp>
+#include <tweedledum/networks/netlist.hpp>
+#include <vector>
+
+TEST_CASE("Decompose 3-controlled Toffoli gate", "[barenco]")
+{
+	using namespace tweedledum;
+	netlist<mcmt_gate> network;
+	network.add_qubit();
+	network.add_qubit();
+	network.add_qubit();
+	network.add_qubit();
+	network.add_gate(gate::mcx, std::vector({0u, 1u, 2u}), std::vector(1, 3u));
+	auto snetwork = barenco_decomposition(network);
+}
