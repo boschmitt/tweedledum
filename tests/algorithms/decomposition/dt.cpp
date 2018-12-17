e5d7c9cec585e9ffef418d556ee8f05b1fc20af1
Bruno Schmitt <bruno.schmitt@epfl.ch>
2018-12-17 21:52:28 +0100

    Forgot these.

--- tests/algorithms/decomposition/dt.cpp
+++ tests/algorithms/decomposition/dt.cpp
@@ -6,6 +6,7 @@
 #include <catch.hpp>
 #include <tweedledum/algorithms/decomposition/dt.hpp>
 #include <tweedledum/gates/mcmt_gate.hpp>
+#include <tweedledum/io/write_qpic.hpp>
 #include <tweedledum/networks/netlist.hpp>
 #include <tweedledum/networks/qubit.hpp>
 #include <vector>
@@ -21,4 +22,16 @@ TEST_CASE("Decompose 2-controlled Z gate", "[dt_decomposition]")
 	                 std::vector<qubit_id>(1, 2u));
 	network.add_gate(gate::mcz, std::vector<qubit_id>({0u, 1u}), std::vector<qubit_id>(1, 2u));
 	auto snetwork = dt_decomposition(network);
+}
+
+TEST_CASE("Decompose toffolli gate with one negative control", "[dt_decomposition]")
+{
+	using namespace tweedledum;
+	netlist<mcmt_gate> network;
+	auto q0 = network.add_qubit();
+	auto q1 = network.add_qubit();
+	auto q2 = network.add_qubit();
+	network.add_gate(gate::mcx, std::vector({!q0, q1}), std::vector({q2}));
+	auto snetwork = dt_decomposition(network);
+	write_qpic(snetwork, "dec.qpic");
 }
--- tests/networks/netlist.cpp
+++ tests/networks/netlist.cpp
@@ -7,39 +7,77 @@
 #include <catch.hpp>
 #include <random>
 #include <string>
-#include <tweedledum/gates/mcst_gate.hpp>
+#include <tweedledum/gates/mcmt_gate.hpp>
+#include <tweedledum/io/write_unicode.hpp>
 #include <tweedledum/networks/netlist.hpp>
 #include <vector>
 
-TEST_CASE("Netlist simple constructor", "[netlist]")
+TEST_CASE("Netlist simple constructors", "[netlist]")
 {
 	using namespace tweedledum;
-	netlist<mcst_gate> network;
 
-	CHECK(network.size() == 0);
+	SECTION("No gates") {
+		netlist<mcmt_gate> network;
+		CHECK(network.size() == 0);
 
-	network.add_qubit("q0");
-	CHECK(network.size() == 2);
-	CHECK(network.num_qubits() == 1);
+		network.add_qubit("q0");
+		network.add_qubit();
+		CHECK(network.size() == 4);
+		CHECK(network.num_qubits() == 2);
+	}
+	SECTION("One gate") {
+		netlist<mcmt_gate> network;
+		auto q0 = network.add_qubit("q0");
+		auto q1 = network.add_qubit();
+		network.add_gate(gate::hadamard, q0);
+		network.add_gate(gate::cx, q0, q1);
+		CHECK(network.size() == 6);
+		CHECK(network.num_qubits() == 2);
+		write_unicode(network);
+	}
+	SECTION("One gate, negative control") {
+		netlist<mcmt_gate> network;
+		auto q0 = network.add_qubit("q0");
+		auto q1 = network.add_qubit();
+		network.add_gate(gate::hadamard, q0);
+		network.add_gate(gate::cx, !q0, q1);
+		CHECK(network.size() == 6);
+		CHECK(network.num_qubits() == 2);
+		write_unicode(network);
+	}
+	SECTION("One gate, negative control") {
+		netlist<mcmt_gate> network;
+		auto q0 = network.add_qubit("q0");
+		auto q1 = network.add_qubit();
+		auto q2 = network.add_qubit();
+		network.add_gate(gate::mcx, std::vector({!q0, q1}), std::vector({q2}));
+		CHECK(network.size() == 7);
+		CHECK(network.num_qubits() == 3);
+		write_unicode(network);
+	}
 
-	network.add_qubit();
-	CHECK(network.size() == 4);
-	CHECK(network.num_qubits() == 2);
 }
 
 TEST_CASE("Netlist const iterators", "[netlist]")
 {
 	using namespace tweedledum;
-	netlist<mcst_gate> network;
+	netlist<mcmt_gate> network;
 	network.add_qubit("q0");
 	network.add_qubit("q1");
 
 	network.foreach_cqubit([](auto qid, auto const& qlabel) {
-		if (qid == 0) {
+		if (qid == qubit_id(0)) {
 			CHECK(qlabel == std::string("q0"));
 		}
-		if (qid == 1) {
+		if (qid == qubit_id(1)) {
 			CHECK(qlabel == std::string("q1"));
 		}
 	});
+
+	network.add_gate(gate::hadamard, "q0");
+	network.add_gate(gate::cx, "q0", "q1");
+	network.add_gate(gate::hadamard, "q0");
+	network.add_gate(gate::cx, "q0", "q1");
+	network.add_gate(gate::cx, "q1", "q0");
+	write_unicode(network);
 }
