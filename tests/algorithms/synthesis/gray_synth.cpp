98686e99de01e1fd57c26a9863069246e7c56efc
Bruno Schmitt <bruno.schmitt@epfl.ch>
2019-02-10 15:48:27 +0100

    Fixing and cleaning up the testcase (remove debug printing)

--- tests/algorithms/synthesis/gray_synth.cpp
+++ tests/algorithms/synthesis/gray_synth.cpp
@@ -6,7 +6,6 @@
 #include <catch.hpp>
 #include <tweedledum/algorithms/synthesis/gray_synth.hpp>
 #include <tweedledum/gates/mcst_gate.hpp>
-#include <tweedledum/io/write_unicode.hpp>
 #include <tweedledum/networks/netlist.hpp>
 #include <tweedledum/utils/angle.hpp>
 #include <tweedledum/utils/parity_terms.hpp>
@@ -23,8 +22,6 @@ TEST_CASE("Check simple example from Amy paper", "[gray_synth]")
 	parities.add_term(0b0011, symbolic_angles::one_eighth);
 
 	auto network = gray_synth<netlist<mcst_gate>>(4, parities);
-	write_unicode(network);
-
 	bit_matrix_rm id_matrix(4, 4);
 	id_matrix.foreach_row([](auto& row, const auto row_index){
 		row[row_index] = 1;
@@ -40,7 +37,6 @@ TEST_CASE("Check simple example from Amy paper", "[gray_synth]")
 			id_matrix.row(t) ^= id_matrix.row(c);
 		}
 	});
-	id_matrix.print();
 }
 
 TEST_CASE("Check with empty parities", "[gray_synth]")
