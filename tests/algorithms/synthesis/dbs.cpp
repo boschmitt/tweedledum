a3db395f4ecfa6b94fc181fa139ee5397793ef5d
Bruno Schmitt <bruno.schmitt@epfl.ch>
2019-04-25 00:11:21 +0200

    Improve tests to use `simulate_classically`.

--- tests/algorithms/synthesis/dbs.cpp
+++ tests/algorithms/synthesis/dbs.cpp
@@ -4,6 +4,7 @@
 | Author(s): Bruno Schmitt, Mathias Soeken
 *-------------------------------------------------------------------------------------------------*/
 #include <catch.hpp>
+#include <tweedledum/algorithms/simulation/simulate_classically.hpp>
 #include <tweedledum/algorithms/synthesis/dbs.hpp>
 #include <tweedledum/algorithms/synthesis/stg.hpp>
 #include <tweedledum/gates/gate_set.hpp>
@@ -19,33 +20,14 @@ TEMPLATE_PRODUCT_TEST_CASE("Decomposition based synthesis", "[dbs][template]",
 	SECTION("Synthesize PRIME(3) - PPRM")
 	{
 		const auto network = dbs<TestType>(permutation, stg_from_pprm());
-		CHECK(network.num_gates() == 6u);
-		CHECK(network.num_qubits() == 3u);
+		for (auto i = 0u; i < permutation.size(); ++i) {
+			CHECK(simulate_classically(network, i) == permutation[i]);
+		}
 	}
 	SECTION("Synthesize PRIME(3) - spectrum")
 	{
 		const auto network = dbs<TestType>(permutation, stg_from_spectrum());
-		CHECK(network.num_gates() == 48u);
+		CHECK(network.num_gates() == 52u);
 		CHECK(network.num_qubits() == 3u);
 	}
 }
-
-// TEST_CASE("Check DBS with PRIME(3) and PPRM", "[dbs]")
-// {
-// 	using namespace tweedledum;
-// 	std::vector<uint32_t> permutation{{0, 2, 3, 5, 7, 1, 4, 6}};
-// 	const auto network = dbs<netlist<mcmt_gate>>(permutation, stg_from_pprm());
-
-// 	CHECK(network.num_gates() == 6u);
-// 	CHECK(network.num_qubits() == 3u);
-// }
-
-// TEST_CASE("Check DBS with PRIME(3) and spectrum", "[dbs]")
-// {
-// 	using namespace tweedledum;
-// 	std::vector<uint32_t> permutation{{0, 2, 3, 5, 7, 1, 4, 6}};
-// 	const auto network = dbs<netlist<mcmt_gate>>(permutation, stg_from_spectrum());
-
-// 	CHECK(network.num_gates() == 48u);
-// 	CHECK(network.num_qubits() == 3u);
-// }
--- tests/algorithms/synthesis/stg.cpp
+++ tests/algorithms/synthesis/stg.cpp
@@ -7,11 +7,12 @@
 #include <kitty/constructors.hpp>
 #include <kitty/dynamic_truth_table.hpp>
 #include <sstream>
+#include <tweedledum/algorithms/simulation/simulate_classically.hpp>
 #include <tweedledum/algorithms/synthesis/stg.hpp>
 #include <tweedledum/gates/mcmt_gate.hpp>
 #include <tweedledum/networks/gg_network.hpp>
-#include <tweedledum/networks/netlist.hpp>
 #include <tweedledum/networks/io_id.hpp>
+#include <tweedledum/networks/netlist.hpp>
 
 namespace tweedledum::detail {
 
@@ -37,8 +38,13 @@ TEMPLATE_PRODUCT_TEST_CASE("Single-target gate synthesis", "[stg][template]", (g
 		kitty::create_from_hex_string(tt, "DA657041");
 		auto [network, map] = detail::circuit_and_map<TestType>(6u);
 		stg_from_pkrm()(network, map, tt);
-		CHECK(network.num_gates() == 10u);
-		CHECK(network.num_qubits() == 6u);
+		for (auto i = 0ull; i < tt.num_bits(); ++i) {
+			auto expected_output = i;
+			if (kitty::get_bit(tt, i)) {
+				expected_output |= (1ull << tt.num_vars());
+			}
+			CHECK(simulate_classically(network, i) == expected_output);
+		}
 	}
 	SECTION("Synthesize using stg_from_pprm")
 	{
@@ -46,8 +52,13 @@ TEMPLATE_PRODUCT_TEST_CASE("Single-target gate synthesis", "[stg][template]", (g
 		kitty::create_from_hex_string(tt, "DA657041");
 		auto [network, map] = detail::circuit_and_map<TestType>(6u);
 		stg_from_pprm()(network, map, tt);
-		CHECK(network.num_gates() == 21u);
-		CHECK(network.num_qubits() == 6u);
+		for (auto i = 0ull; i < tt.num_bits(); ++i) {
+			auto expected_output = i;
+			if (kitty::get_bit(tt, i)) {
+				expected_output |= (1ull << tt.num_vars());
+			}
+			CHECK(simulate_classically(network, i) == expected_output);
+		}
 	}
 	SECTION("Synthesize stg_from_spectrum")
 	{
@@ -55,64 +66,7 @@ TEMPLATE_PRODUCT_TEST_CASE("Single-target gate synthesis", "[stg][template]", (g
 		kitty::create_from_hex_string(tt, "DA657041");
 		auto [network, map] = detail::circuit_and_map<TestType>(6u);
 		stg_from_spectrum()(network, map, tt);
-		CHECK(network.num_gates() == 185u);
+		CHECK(network.num_gates() == 108u);
 		CHECK(network.num_qubits() == 6u);
 	}
 }
-
-// // TODO: improve test case
-// SECTION("Synthesize a simple function into a quantum network using stg_from_exact_esop",
-//           "[stg]")
-// {
-// 	kitty::dynamic_truth_table tt(3);
-// 	kitty::create_from_binary_string(tt, "10000001");
-// 	auto [network, map] = detail::circuit_and_map<netlist<mcmt_gate>>(4u);
-// 	stg_from_exact_esop()(network, map, tt);
-
-// 	CHECK(network.num_gates() == 8u);
-// 	CHECK(network.num_qubits() == 4u);
-
-// 	// for (auto i = 0ull; i < tt.num_bits(); ++i) {
-// 	// 	auto expected_output = i;
-// 	// 	if (kitty::get_bit(tt, i)) {
-// 	// 		expected_output |= 1 << tt.num_vars();
-// 	// 	}
-// 	// 	CHECK(simulate_pattern_classical(network, i) == expected_output);
-// 	// }
-// }
-
-// SECTION("Synthesize a simple function into a quantum network using stg_from_pkrm",
-//           "[stg]")
-// {
-// 	kitty::dynamic_truth_table tt(5);
-// 	kitty::create_from_hex_string(tt, "DA657041");
-// 	auto [network, map] = detail::circuit_and_map<netlist<mcmt_gate>>(6u);
-// 	stg_from_pkrm()(network, map, tt);
-
-// 	CHECK(network.num_gates() == 10u);
-// 	CHECK(network.num_qubits() == 6u);
-// }
-
-// TEST_CASE("Synthesize a simple function into a quantum network using stg_from_pprm",
-//           "[stg]")
-// {
-// 	kitty::dynamic_truth_table tt(5);
-// 	kitty::create_from_hex_string(tt, "DA657041");
-// 	auto [network, map] = detail::circuit_and_map<netlist<mcmt_gate>>(6u);
-// 	stg_from_pprm()(network, map, tt);
-
-// 	CHECK(network.num_gates() == 21u);
-// 	CHECK(network.num_qubits() == 6u);
-// }
-
-// TEST_CASE("Synthesize a simple function into a quantum network using stg_from_spectrum",
-//           "[stg]")
-// {
-// 	kitty::dynamic_truth_table tt(5);
-// 	kitty::create_from_hex_string(tt, "DA657041");
-// 	auto [network, map] = detail::circuit_and_map<netlist<mcmt_gate>>(6u);
-// 	stg_from_spectrum()(network, map, tt);
-
-// 	CHECK(network.num_gates() == 185u);
-// 	CHECK(network.num_qubits() == 6u);
-// }
