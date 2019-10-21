aadf6632ea6dbf250bbaf156a9cb38fab006d815
Bruno Schmitt <bruno.schmitt@epfl.ch>
2019-10-21 17:55:06 +0200

    Integrating bill

--- include/tweedledum/utils/hash.hpp
+++ include/tweedledum/utils/hash.hpp
@@ -4,42 +4,32 @@
 *-------------------------------------------------------------------------------------------------*/
 #pragma once
 
+#include <functional>
 #include <set>
-#include <utility>
 
-namespace std {
-
-template<class T>
-inline void hash_combine(std::size_t& seed, T const& v)
-{
-	seed ^= std::hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-}
+namespace tweedledum {
 
 template<typename T>
-struct hash<set<T>> {
-	using argument_type = set<T>;
+struct hash : public std::hash<T> {};
+
+template<>
+struct hash<std::set<uint32_t>> {
+	using argument_type = std::set<uint32_t>;
 	using result_type = size_t;
+
+	void combine(std::size_t& seed, uint32_t const& v) const
+	{
+		seed ^= std::hash<uint32_t>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+	}
+
 	result_type operator()(argument_type const& in) const
 	{
 		result_type seed = 0;
 		for (auto& element : in) {
-			hash_combine(seed, element);
+			combine(seed, element);
 		}
 		return seed;
 	}
 };
 
-template<>
-struct hash<pair<uint32_t, uint32_t>> {
-	using argument_type = pair<uint32_t, uint32_t>;
-	using result_type = size_t;
-	result_type operator()(argument_type const& in) const
-	{
-		result_type seed = 0;
-		hash_combine(seed, in.first);
-		hash_combine(seed, in.second);
-		return seed;
-	}
-};
-
-} // namespace std
+} // namespace tweedledum
