182310d64b77b32ac85c7b6e05682a835932b0ad
Bruno Schmitt <bruno.schmitt@epfl.ch>
2019-11-04 19:19:17 +0100

    Improve gate cancellation by taken into account dependecy.

--- include/tweedledum/algorithms/optimization/gate_cancellation.hpp
+++ include/tweedledum/algorithms/optimization/gate_cancellation.hpp
@@ -5,7 +5,14 @@
 #pragma once
 
 #include "../generic/remove_marked.hpp"
+#include "../../gates/gate_lib.hpp"
+
+#include <vector>
+#include <set>
 #include <cassert>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
 
 namespace tweedledum {
 
@@ -20,25 +27,49 @@ Network gate_cancellation(Network const& network)
 	network.clear_values();
 	network.foreach_gate([&](auto const& node) {
 		std::vector<link_type> children;
-		bool do_remove = false;
-		network.foreach_child(node, [&](auto child_index) {
-			if (!children.empty() && children.back() != child_index) {
-				do_remove = false;
-				return false;
-			}
-			children.push_back(child_index);
-			auto& child = network.get_node(child_index);
-			if (network.value(child)) {
-				return true;
-			}
-			if (node.gate.is_adjoint(child.gate)) {
-				do_remove = true;
-			}
-			return true;
+		std::set<uint32_t> qubits;
+		network.foreach_child(node, [&](link_type child_index, io_id io) {
+			children.emplace_back(child_index);
+			qubits.insert(io.index());
 		});
+		size_t i = 0;
+		size_t i_end = children.size();
+		link_type adj_node;
+		bool do_remove = false;
+		while(i < children.size()) {
+			auto prev_node = network.get_node(children.at(i));
+			if (prev_node.gate.is(gate_lib::input)) {
+				break;
+			}
+			if (network.value(prev_node) == 0) {
+				if (node.gate.is_adjoint(prev_node.gate)) {
+					adj_node = children.at(i);
+					do_remove = true;
+					++i;
+					continue;
+				}
+				if (node.gate.is_dependent(prev_node.gate)) {
+					do_remove = false;
+					break;
+				}
+			}
+			network.foreach_child(prev_node, [&](link_type child_index, io_id io) {
+				if (qubits.find(io.index()) == qubits.end()) {
+					return;
+				}
+				children.emplace_back(child_index);
+			});
+			++i;
+			if (i == i_end) {
+				if (do_remove) {
+					break;
+				}
+				i_end = children.size();
+			}
+		}
 		if (do_remove) {
 			network.set_value(node, 1);
-			network.set_value(network.get_node(children.back()), 1);
+			network.set_value(network.get_node(adj_node), 1);
 			num_deletions += 2;
 			return;
 		}
