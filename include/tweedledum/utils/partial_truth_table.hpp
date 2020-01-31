b0d62300e1b544c3fd96b15cad62a3dc68c89eda
Heinz Riener <heinz.riener@gmail.com>
2020-01-31 16:54:07 +0100

    partial truth tables.

---
+++ include/tweedledum/utils/partial_truth_table.hpp
@@ -0,0 +1,173 @@
+#pragma once
+
+#include <kitty/bit_operations.hpp>
+#include <kitty/dynamic_truth_table.hpp>
+#include <kitty/operations.hpp>
+#include <kitty/operators.hpp>
+#include <kitty/print.hpp>
+#include <iostream>
+
+namespace tweedledum
+{
+
+inline constexpr std::uint32_t ilog2( std::uint32_t const n )
+{
+  return ( n > 1 ) ?  1+log2( n >> 1 ) : 0;
+}
+
+inline std::uint32_t next_pow2( std::uint32_t n )
+{
+  --n;
+  n |= n >> 1;
+  n |= n >> 2;
+  n |= n >> 4;
+  n |= n >> 8;
+  n |= n >> 16;
+  ++n;
+  return n;
+}
+
+/*! \brief A truth table interface to represent bit-vectors with less than 2^n elements. */
+class partial_truth_table
+{
+public:
+  friend void print_binary( partial_truth_table const& tt, std::ostream& os );
+
+public:
+  static partial_truth_table create_from_binary_string( std::string const& binary )
+  {
+    assert( binary.size() > 0u );
+
+    partial_truth_table tt( binary.size() );
+
+    for ( uint32_t index = 0u; index < binary.size(); ++index )
+    {
+      if ( binary.at( index ) == '0' )
+      {
+        tt.clear_bit( index );
+      }
+      else if ( binary.at( index ) == '1' )
+      {
+        tt.set_bit( index );
+      }
+      else
+      {
+        assert( false && "unknown symbol in binary string" );
+      }
+    }
+
+    return tt;
+  }
+
+public:
+  explicit partial_truth_table( uint32_t const num_bits )
+    : _num_bits( num_bits )
+    , _bits( kitty::dynamic_truth_table( ilog2( next_pow2( num_bits ) ) ) )
+    , _mask( kitty::dynamic_truth_table( ilog2( next_pow2( num_bits ) ) ) )
+  {
+    /* initialize the mask */
+    for ( auto i = _num_bits; i < _mask.num_bits(); ++i )
+      kitty::set_bit( _mask, i );
+    _mask = ~_mask;
+  }
+
+  explicit partial_truth_table( kitty::dynamic_truth_table const& tt, uint32_t num_bits )
+    : _num_bits( num_bits )
+    , _bits( tt )
+    , _mask( tt )
+  {
+    assert( 1 << tt.num_vars() >= num_bits );
+    for ( uint32_t index = 0u; index < _num_bits; ++index )
+    {
+      kitty::set_bit( _mask, index );
+    }
+  }
+
+  void set_bit( uint32_t index )
+  {
+    assert( index < _num_bits );
+    kitty::set_bit( _bits, index );
+  }
+
+  void clear_bit( uint32_t index )
+  {
+    assert( index < _num_bits );
+    kitty::clear_bit( _bits, index );
+  }
+
+  bool get_bit( uint32_t index ) const
+  {
+    assert( index < _num_bits );
+    return kitty::get_bit( _bits, index );
+  }
+
+  uint64_t count_ones() const
+  {
+    return kitty::count_ones( _bits );
+  }
+
+  partial_truth_table operator&( partial_truth_table const& tt ) const
+  {
+    assert( _num_bits == tt.num_bits() );
+    partial_truth_table r( _num_bits );
+    r._bits = kitty::binary_and( _bits, tt._bits );
+    r._mask = _mask;
+    return r;
+  }
+
+  partial_truth_table operator|( partial_truth_table const& tt ) const
+  {
+    assert( _num_bits == tt.num_bits() );
+    partial_truth_table r( _num_bits );
+    r._bits = kitty::binary_or( _bits, tt._bits );
+    r._mask = _mask;
+    return r;
+  }
+
+  partial_truth_table operator^( partial_truth_table const& tt ) const
+  {
+    assert( _num_bits == tt.num_bits() );
+    partial_truth_table r( _num_bits );
+    r._bits = kitty::binary_xor( _bits, tt._bits );
+    r._mask = _mask;
+    return r;
+  }
+
+  partial_truth_table operator~() const
+  {
+    partial_truth_table r( _num_bits );
+    r._bits = kitty::binary_and( ~_bits, _mask );
+    r._mask = _mask;
+    return r;
+  }
+
+  bool operator==( partial_truth_table const& tt ) const
+  {
+    assert( _num_bits == tt.num_bits() );
+    return ( _bits == tt._bits );
+  }
+
+  uint32_t num_bits() const
+  {
+    return _num_bits;
+  }
+
+  std::pair<kitty::dynamic_truth_table,kitty::dynamic_truth_table> to_isop() const
+  {
+    return {_bits, _mask};
+  }
+
+public:
+  uint32_t _num_bits;
+  kitty::dynamic_truth_table _bits;
+  kitty::dynamic_truth_table _mask;
+}; /* partial_truth_table */
+
+inline void print_binary( partial_truth_table const& tt, std::ostream& os = std::cout )
+{
+  kitty::print_binary( tt._bits, os );
+  os << ':';
+  os << tt._num_bits;
+}
+
+} // namespace tweedledum
---
+++ tests/utils/partial_truth_table.cpp
@@ -0,0 +1,91 @@
+/* author: Fereshte */
+#include <catch.hpp>
+#include <tweedledum/utils/partial_truth_table.hpp>
+#include <kitty/constructors.hpp>
+#include <sstream>
+
+using namespace tweedledum;
+
+TEST_CASE("Create partial truth table", "[partial_truth_table]")
+{
+  partial_truth_table ptt( 10u );
+  CHECK( ptt.num_bits() == 10u );
+  CHECK( ptt.count_ones() == 0u );
+}
+
+TEST_CASE("Create from dynamic truth table", "[partial_truth_table]")
+{
+  kitty::dynamic_truth_table mt0( 2 ); /* num_vars = 2 --> size = 2^2 = 4 */
+  kitty::create_from_binary_string( mt0, "0000" );
+  partial_truth_table ptt_mt0( mt0, 3 ); /* num_bits = 3 --> size = 3 */
+  CHECK( ptt_mt0.num_bits() == 3u );
+
+  kitty::dynamic_truth_table mt1( 2 );
+  kitty::create_from_binary_string( mt1, "0111" );
+  partial_truth_table ptt_mt1( mt1, 3 );
+  CHECK( ptt_mt1.num_bits() == 3u );
+}
+
+// TEST_CASE("Create from too small truth table", "[partial_truth_table]")
+// {
+//   kitty::dynamic_truth_table tt( 2 );
+//   partial_truth_table ptt( tt, 5u );
+//   CHECK( ptt.num_bits() == 5u ); /* should fail */
+// }
+
+TEST_CASE("Create from string", "[partial_truth_table]")
+{
+  auto const mt0 = partial_truth_table::create_from_binary_string( "000" );
+  CHECK( mt0.num_bits() == 3u );
+  
+  auto const mt1 = partial_truth_table::create_from_binary_string( "111" );
+  CHECK( mt1.num_bits() == 3u );
+}
+
+TEST_CASE("Binary operations", "[partial_truth_table]")
+{
+  auto const a = partial_truth_table::create_from_binary_string( "0001" );
+  auto const b = partial_truth_table::create_from_binary_string( "1011" );
+  CHECK( a.num_bits() == 4u );
+  CHECK( b.num_bits() == 4u );
+
+  CHECK( a.count_ones() == 1u );
+  CHECK( b.count_ones() == 3u );
+  
+  CHECK( ( a & b ) == partial_truth_table::create_from_binary_string( "0001" ) );
+  CHECK( ( a | b ) == partial_truth_table::create_from_binary_string( "1011" ) );
+  CHECK( ( a ^ b ) == partial_truth_table::create_from_binary_string( "1010" ) );
+  CHECK( ( ~a ) == partial_truth_table::create_from_binary_string( "1110" ) );
+  CHECK( ( ~b ) == partial_truth_table::create_from_binary_string( "0100" ) );
+}
+
+TEST_CASE("Bit operations", "[partial_truth_table]")
+{
+  auto a = partial_truth_table::create_from_binary_string( "1100" );
+  a.clear_bit( 0 );
+  CHECK( ( a ) == partial_truth_table::create_from_binary_string( "0100" ) );
+
+  a.clear_bit( 1 );  
+  CHECK( ( a ) == partial_truth_table::create_from_binary_string( "0000" ) );
+
+  a.set_bit( 2 );  
+  CHECK( ( a ) == partial_truth_table::create_from_binary_string( "0010" ) );  
+
+  a.set_bit( 3 );  
+  CHECK( ( a ) == partial_truth_table::create_from_binary_string( "0011" ) );    
+
+  CHECK( a.get_bit( 0 ) == false ); 
+  CHECK( a.get_bit( 1 ) == false ); 
+  CHECK( a.get_bit( 2 ) == true ); 
+  CHECK( a.get_bit( 3 ) == true ); 
+}
+
+TEST_CASE("Print binary", "[partial_truth_table]")
+{
+  auto const a = partial_truth_table::create_from_binary_string( "110" );
+
+  std::stringstream ss;
+  print_binary( a, ss );
+
+  CHECK( ss.str() == "0011:3" ); /* TODO: should be "011" */
+}
