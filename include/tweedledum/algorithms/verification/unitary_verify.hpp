83afcac6751cd4d80ef66563f48cf31fd4210ebb
Bruno Schmitt <bruno.schmitt@epfl.ch>
2020-03-30 12:56:23 +0200

    Add equivalence verification by using unitaries

---
+++ include/tweedledum/algorithms/verification/unitary_verify.hpp
@@ -0,0 +1,28 @@
+/*-------------------------------------------------------------------------------------------------
+| This file is distributed under the MIT License.
+| See accompanying file /LICENSE for details.
+*------------------------------------------------------------------------------------------------*/
+#pragma once
+
+#include "../../networks/unitary.hpp"
+
+namespace tweedledum {
+
+/* \brief Check if two networks are equivalent using unitaries
+ *
+ * Needless to say that this method is not scalable at all! But its good to equivalent check small
+ * examples and test cases.
+ * 
+ * \params rtol Relative tolerance (default: 1e-05)
+ * \params atol Absolute tolerance (default: 1e-08)
+ */
+template<typename Network0, typename Network1>
+bool unitary_verify(Network0 const& network0, Network1 const& network1, double const rtol = 1e-05,
+	                    double const atol = 1e-08)
+{
+	unitary u0(network0);
+	unitary u1(network1);
+	return u0.is_apprx_equal(u1, rtol, atol);
+}
+
+} // namespace tweedledum
