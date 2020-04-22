b1a6832bbc6b92a3afe9e7cb19c7a3447e419f57
Bruno Schmitt <bruno.schmitt@epfl.ch>
2020-04-22 20:29:14 +0200

    Update kitty

--- lib/kitty/kitty/implicant.hpp
+++ lib/kitty/kitty/implicant.hpp
@@ -37,6 +37,7 @@
 
 #include "algorithm.hpp"
 #include "cube.hpp"
+#include "traits.hpp"
 
 namespace kitty
 {
@@ -51,7 +52,7 @@ std::vector<uint32_t> get_minterms( const TT& tt )
   std::vector<uint32_t> m;
   m.reserve( count_ones( tt ) );
   for_each_one_bit( tt, [&m]( auto index ) {
-    m.emplace_back( index );
+    m.emplace_back( static_cast<uint32_t>( index ) );
   } );
   return m;
 }
@@ -235,6 +236,8 @@ inline std::vector<cube> get_prime_implicants_morreale( const std::vector<uint32
 template<typename TT>
 std::vector<cube> get_prime_implicants_morreale( const TT& tt )
 {
+  static_assert( is_complete_truth_table<TT>::value, "Can only be applied on complete truth tables." );
+
   return get_prime_implicants_morreale( get_minterms( tt ), tt.num_vars() );
 }
 } // namespace kitty
