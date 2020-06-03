007338504f0d29b8a45d1aedc58e9b7d75400a50
Bruno Schmitt <bruno.schmitt@epfl.ch>
2020-06-03 21:58:48 +0200

    Changes to setup.py and settle on a version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name="tweedledum",
-    version="0.1",
+    version="0.1b0",
     description="A library for synthesizing and manipulating quantum circuits",
     url="https://github.com/boschmitt/tweedledum",
     author='Bruno Schmitt',
@@ -25,10 +25,19 @@ setup(
     packages=['tweedledum', 'tweedledum.parser'],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.5",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: C++",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
     ]
 )
