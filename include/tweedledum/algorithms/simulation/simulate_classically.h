7cc243c5839339576c563142fdae70a579f19bb6
Bruno Schmitt <bruno.schmitt@epfl.ch>
2020-06-10 11:17:23 +0200

    Allow classical simulator to simulate truth tables

--- include/tweedledum/algorithms/simulation/simulate_classically.h
+++ include/tweedledum/algorithms/simulation/simulate_classically.h
@@ -15,21 +15,31 @@
 
 namespace tweedledum {
 
-using TruthTable = kitty::dynamic_truth_table;
-
 template<typename WordType>
 inline DynamicBitset<WordType> simulate_classically(
     Circuit const& circuit, DynamicBitset<WordType> pattern)
 {
 	assert(circuit.num_qubits() == pattern.size());
 	for (auto const& inst : circuit) {
-		assert(inst.kind() == "x");
 		bool execute = true;
-		std::for_each(
-		    inst.begin(), inst.end() - 1, [&](WireRef const& wire) {
-			    auto bit = pattern[wire.uid()];
-			    execute &= bit ^ wire.polarity();
-		    });
+		if (inst.is<GateLib::TruthTable>()) {
+			auto const& tt = inst.cast<GateLib::TruthTable>();
+			// TODO: refactor:
+			uint32_t i = 0;
+			uint32_t pos = 0u;
+			std::for_each(inst.begin(), inst.end() - 1,
+			[&](WireRef const& wire) {
+				pos |= (pattern[wire.uid()] << i);
+				++i;
+			});
+			execute &= kitty::get_bit(tt.truth_table(), pos);
+		} else if (inst.is<GateLib::X>()) {
+			std::for_each(inst.begin(), inst.end() - 1,
+			[&](WireRef const& wire) {
+				auto bit = pattern[wire.uid()];
+				execute &= bit ^ wire.polarity();
+			});
+		}
 		if (execute) {
 			pattern.flip(inst.target().uid());
 		}
