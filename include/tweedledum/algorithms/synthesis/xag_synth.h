d2427ee6a6694b85e999ace73b4495b2e7af27f5
Bruno Schmitt <bruno.schmitt@epfl.ch>
2020-06-19 10:52:34 +0200

    Rafctoring and bug fixing

--- include/tweedledum/algorithms/synthesis/xag_synth.h
+++ include/tweedledum/algorithms/synthesis/xag_synth.h
@@ -8,6 +8,7 @@
 #include "../../ir/GateLib.h"
 #include "../../ir/Wire.h"
 
+#include <array>
 #include <cassert>
 #include <mockturtle/networks/xag.hpp>
 #include <mockturtle/utils/node_map.hpp>
@@ -17,335 +18,269 @@ namespace tweedledum {
 #pragma region Implementation details
 namespace xag_synth_detail {
 
-using XAG = mockturtle::xag_network;
-using XAGNode = typename mockturtle::xag_network::node;
-using XAGSignal = typename mockturtle::xag_network::signal;
-// LTFI = Linear Transitive Fanin
-using LTFI = std::vector<XAGSignal>;
-
 struct ParityAnd {
-	std::vector<WireRef> in0;
-	std::vector<WireRef> in1;
-	std::vector<WireRef> in01;
-	WireRef target;
-	bool cleanup;
+	std::vector<uint32_t> in0;
+	std::vector<uint32_t> in1;
+	std::vector<uint32_t> in01;
 	std::array<bool, 2> is_complemented;
-
-	ParityAnd() : target(WireRef::invalid()), cleanup(false), is_complemented({{false, false}}) {}
+	uint32_t ref_count;
 };
 
-struct State {
-	XAG const& xag;
-	mockturtle::node_map<WireRef, XAG> to_qubit; // XAG node -> qubit
-	mockturtle::node_map<LTFI, XAG> ltfi; // XAG node -> ltfi
-
-	// std::vector<WireRef> const& qubits;
-	Circuit& circuit;
-
-	State(XAG const& xag, Circuit& circuit)
-	    : xag(xag), to_qubit(xag, WireRef::invalid()), ltfi(xag),
-	      circuit(circuit)
-	{}
+enum class Action : uint8_t {
+	compute,
+	cleanup,
 };
 
-void preprocess_inputs(State& state, std::vector<WireRef> const& qubits)
-{
-	uint32_t index = 0;
-	state.xag.foreach_pi([&](XAGNode const& node) {
-		// fmt::print("{}\n", index);
-		state.to_qubit[node] = qubits[index];
-		state.ltfi[node].emplace_back(state.xag.make_signal(node));
-		index += 1;
-	});
-}
+struct Step {
+	Action action;
+	uint32_t node;
+	Step(Action a, uint32_t n) : action(a), node(n) {}
+};
 
-void preprocess_outputs(State& state, std::vector<WireRef> const& qubits)
+std::vector<ParityAnd> collapse_xag(mockturtle::xag_network const& xag)
 {
-	uint32_t index = state.xag.num_pis();
-	state.xag.foreach_po([&](XAGSignal const& signal) {
-		XAGNode const& node = state.xag.get_node(signal);
-		if (state.xag.is_and(node)) {
-			if (state.to_qubit[signal] == WireRef::invalid()) {
-				state.to_qubit[signal] = qubits[index];
-			}
-		}
-		index += 1;
+	using XAG = mockturtle::xag_network;
+	using Node = typename XAG::node;
+	using Signal = typename XAG::signal;
+	using LTFI = std::vector<Signal>;
+
+	// Preprocess:
+	// - Compute LTFI
+	// - Count references to AND nodes
+	mockturtle::node_map<LTFI, XAG> ltfi(xag);
+	xag.foreach_pi([&](Node const& node, uint32_t index) {
+		ltfi[node].emplace_back(xag.make_signal(node));
+		xag.set_value(node, index + 1);
+		return;
 	});
-	index = state.xag.num_pis();
-	state.xag.foreach_po([&](XAGSignal const& signal) {
-		XAGNode const& node = state.xag.get_node(signal);
-		if (!state.xag.is_xor(node)) {
-			index += 1;
+	uint32_t num_and = 0;
+	xag.foreach_gate([&](Node const& node) {
+		std::array<LTFI const*, 2> node_ltfi;
+		xag.foreach_fanin(node, [&](Signal const signal, uint32_t i) {
+			node_ltfi[i] = &(ltfi[signal]);
+		});
+		if (xag.is_and(node)) {
+			ltfi[node].emplace_back(xag.make_signal(node));
+			xag.set_value(node, num_and + 1 + xag.num_pis());
+			num_and += 1;
 			return;
 		}
-		if (state.to_qubit[signal] == WireRef::invalid()) {
-			state.to_qubit[signal] = qubits[index];
-		}
-		auto test = state.ltfi[node];
-		std::reverse(test.begin(), test.end());
-		// for (XAGSignal in : state.ltfi[node]) {
-		for (XAGSignal in : test) {
-			if (!(state.to_qubit[in] == WireRef::invalid())) {
-				continue;
-			}
-			XAGNode in_node = state.xag.get_node(in);
-			if (!state.xag.is_and(in_node)) {
-				continue;
-			}
-			state.to_qubit[signal] = qubits[index];
-			state.to_qubit[in_node] = qubits[index];
-			break;
-		}
-		index += 1;
+		// The node is then a XOR
+		std::set_symmetric_difference(node_ltfi[0]->cbegin(),
+		    node_ltfi[0]->cend(), node_ltfi[1]->cbegin(),
+		    node_ltfi[1]->cend(), std::back_inserter(ltfi[node]));
+		assert(ltfi[node].size());
 	});
-}
 
-void compute_ltfi(
-    XAG const& xag, XAGNode const& node, mockturtle::node_map<LTFI, XAG>& ltfi)
-{
-	if (xag.is_and(node)) {
-		ltfi[node].emplace_back(xag.make_signal(node));
-		return;
-	}
-	std::array<XAGSignal, 2> fanins;
-	xag.foreach_fanin(node, [&](XAGSignal const& signal, uint32_t i) {
-		fanins.at(i) = signal;
-	});
-	auto const& ltfi_in0 = ltfi[fanins.at(0)];
-	auto const& ltfi_in1 = ltfi[fanins.at(1)];
-	assert(!ltfi_in0.empty());
-	assert(!ltfi_in1.empty());
-	std::set_symmetric_difference(ltfi_in0.begin(), ltfi_in0.end(),
-	    ltfi_in1.begin(), ltfi_in1.end(), std::back_inserter(ltfi[node]));
-	assert(
-	    ltfi[node].size()
-	    && "If the size is empty, then the XAG was not properly optimized!");
-}
-
-uint32_t analyze_xag(XAG const& xag, mockturtle::node_map<LTFI, XAG>& ltfi)
-{
-	uint32_t num_ands = 0u;
-	xag.clear_values();
-	xag.foreach_gate([&](XAGNode const& node) {
-		compute_ltfi(xag, node, ltfi);
-		if (xag.is_and(node)) {
-			num_ands += 1;
-		}
-	});
-	return num_ands;
-}
-
-void compute_inputs(State& state, XAGNode const& node, ParityAnd& gate)
-{
-	std::array<XAGSignal, 2> fanins;
-	bool both_xor = true;
-	state.xag.foreach_fanin(
-	    node, [&](XAGSignal const signal, uint32_t index) {
-		    XAGNode const& fanin = state.xag.get_node(signal);
-		    both_xor &= state.xag.is_xor(fanin);
-		    fanins.at(index) = signal;
-	    });
-
-	LTFI const* ltfi_0 = &(state.ltfi[fanins.at(0)]);
-	LTFI const* ltfi_1 = &(state.ltfi[fanins.at(1)]);
-	if ((ltfi_0->size() == 1) && (ltfi_1->size() == 1)) {
-		gate.in0.push_back(state.to_qubit[ltfi_0->at(0)]);
-		gate.in1.push_back(state.to_qubit[ltfi_1->at(0)]);
-		return;
-	}
-	// Normalization
-	if (ltfi_0->size() < ltfi_1->size()) {
-		std::swap(ltfi_0, ltfi_1);
-		std::swap(fanins.at(0), fanins.at(1));
-	}
-	//
-	auto in0_begin = ltfi_0->cbegin();
-	auto in0_end = ltfi_0->cend();
-	auto in1_begin = ltfi_1->cbegin();
-	auto in1_end = ltfi_1->cend();
-	while (in0_begin != in0_end && in1_begin != in1_end) {
-		if (*in0_begin == *in1_begin) {
-			gate.in01.emplace_back(state.to_qubit[*in0_begin]);
-			++in0_begin;
-			++in1_begin;
-		} else if (*in0_begin < *in1_begin) {
-			gate.in0.emplace_back(state.to_qubit[*in0_begin]);
-			++in0_begin;
-		} else {
-			gate.in1.emplace_back(state.to_qubit[*in1_begin]);
-			++in1_begin;
-		}
-	}
-	while (in0_begin != in0_end) {
-		gate.in0.emplace_back(state.to_qubit[*in0_begin]);
-		++in0_begin;
-	}
-	while (in1_begin != in1_end) {
-		gate.in1.emplace_back(state.to_qubit[*in1_begin]);
-		++in1_begin;
-	}
-}
-
-void compute_gate(
-    State& state, XAGNode const& node, std::vector<ParityAnd>& gates)
-{
-	// Compute inputs
-	ParityAnd& gate = gates.emplace_back();
-	compute_inputs(state, node, gate);
-	WireRef target = state.to_qubit[node];
-	if (target == WireRef::invalid()) {
-		target = state.circuit.request_ancilla();
-		gate.cleanup = true;
-	}
-	gate.target = target;
-	state.to_qubit[node] = target;
-	state.xag.foreach_fanin(node,
-	[&](XAGSignal const& signal, uint32_t i) {
-		if (state.xag.is_complemented(signal)) {
-			gate.is_complemented[i] = true;
-		} 
-	});
-}
-
-void choose_target(std::vector<uint32_t>& usage, std::vector<WireRef>& qubits)
-{
-	if (qubits.size() == 1) {
-		return;
-	}
-	uint32_t chosen_qubit = 0;
-	uint32_t max = usage.at(qubits[chosen_qubit].uid());
-	for (uint32_t i = 1; i < qubits.size(); ++i) {
-		if (max < usage.at(qubits[i].uid())) {
-			chosen_qubit = i;
-			max = usage.at(qubits[i].uid());
-		}
-	}
-	usage.at(qubits[chosen_qubit].uid()) += 1;
-	std::swap(qubits[chosen_qubit], qubits.back());
-}
-
-InstRef compute_parity(Circuit& circuit, std::vector<WireRef> const& qubits)
-{
-	if (qubits.size() == 1) {
-		return InstRef::invalid();
-	}
-	return circuit.create_instruction(GateLib::Parity(), {qubits});
-}
-
-void synthesize(
-    Circuit& circuit, std::vector<WireRef> const& qubits, XAG const& xag)
-{
-	State state(xag, circuit);
-	preprocess_inputs(state, qubits);
-	uint32_t const num_ands = analyze_xag(state.xag, state.ltfi);
-	preprocess_outputs(state, qubits);
-
-	// Create a Network with ParityAnd gates
+	// Create the collapsed XAG
 	std::vector<ParityAnd> gates;
-	gates.reserve(num_ands);
-	xag.foreach_gate([&](XAGNode const& node) {
+	gates.reserve(1 + xag.num_pis() + num_and + xag.num_pos());
+	xag.foreach_node([&](Node const& node) {
+		// FIXME: seems like the constant node is considered a PI!!
+		if (xag.is_pi(node)) {
+			gates.emplace_back();
+			return;
+		}
 		if (!xag.is_and(node)) {
 			return;
 		}
-		compute_gate(state, node, gates);
+		auto& gate = gates.emplace_back();
+		gate.ref_count = 0;
+		std::array<LTFI const*, 2> node_ltfi;
+		xag.foreach_fanin(node, [&](Signal const signal, uint32_t i) {
+			node_ltfi[i] = &(ltfi[signal]);
+			gate.is_complemented[i] = xag.is_complemented(signal);
+		});
+		auto in0_begin = node_ltfi[0]->cbegin();
+		auto in0_end = node_ltfi[0]->cend();
+		auto in1_begin = node_ltfi[1]->cbegin();
+		auto in1_end = node_ltfi[1]->cend();
+		while (in0_begin != in0_end && in1_begin != in1_end) {
+			if (*in0_begin == *in1_begin) {
+				uint32_t id = xag.value(xag.get_node(*in0_begin));
+				gate.in01.emplace_back(id);
+				gates[id].ref_count += 1;
+				++in0_begin;
+				++in1_begin;
+			} else if (*in0_begin < *in1_begin) {
+				uint32_t id = xag.value(xag.get_node(*in0_begin));
+				gate.in0.emplace_back(id);
+				gates[id].ref_count += 1;
+				++in0_begin;
+			} else {
+				uint32_t id = xag.value(xag.get_node(*in1_begin));
+				gate.in1.emplace_back(id);
+				gates[id].ref_count += 1;
+				++in1_begin;
+			}
+		}
+		while (in0_begin != in0_end) {
+			uint32_t id = xag.value(xag.get_node(*in0_begin));
+			gate.in0.emplace_back(id);
+			gates[id].ref_count += 1;
+			++in0_begin;
+		}
+		while (in1_begin != in1_end) {
+			uint32_t id = xag.value(xag.get_node(*in1_begin));
+			gate.in1.emplace_back(id);
+			gates[id].ref_count += 1;
+			++in1_begin;
+		}
 	});
+	xag.foreach_po([&](Signal const& signal) {
+		auto& gate = gates.emplace_back();
+		for (Signal const input : ltfi[signal]) {
+			uint32_t id = xag.value(xag.get_node(input));
+			gate.in0.emplace_back(id);
+			gates[id].ref_count += 1;
+		}
+		gate.is_complemented[0] = xag.is_complemented(signal);
+	});
+	return gates;
+}
 
-	 std::vector<uint32_t> usage(circuit.num_wires(), 0);
-	// Create a network with Parity and Toffoli gates
-	for (ParityAnd& gate : gates) {
-		// Compute inputs
-		choose_target(usage, gate.in0);
-		compute_parity(circuit, gate.in0);
-		if (!gate.in01.empty()) {
-			choose_target(usage, gate.in01);
-			compute_parity(circuit, gate.in01);
-			gate.in1.push_back(gate.in01.back());
-			circuit.create_instruction(GateLib::X(), {gate.in01.back()}, gate.in0.back());
-		} else {
-			choose_target(usage, gate.in1);
-		}
-		compute_parity(circuit, gate.in1);
-		// Compute Toffoli
-		WireRef c0 = gate.is_complemented[0] ? !gate.in0.back() : gate.in0.back();
-		WireRef c1 = gate.is_complemented[1] ? !gate.in1.back() : gate.in1.back();
-		circuit.create_instruction(GateLib::X(), {c0, c1}, gate.target);
-		compute_parity(circuit, gate.in1);
-		if (!gate.in01.empty()) {
-			circuit.create_instruction(GateLib::X(), {gate.in01.back()}, gate.in0.back());
-			compute_parity(circuit, gate.in01);
-		}
-		compute_parity(circuit, gate.in0);
+
+void compute_parity(Circuit& circuit, std::vector<WireRef> const& qubits)
+{
+	if (qubits.size() == 1) {
+		return;
 	}
-	// Compute XOR POs
-	xag.foreach_po([&](XAGSignal const& signal) {
-		XAGNode const& node = xag.get_node(signal);
+	circuit.create_instruction(GateLib::Parity(), qubits);
+}
 
-		if (!xag.is_xor(node) || state.to_qubit[signal] == WireRef::invalid()) {
-			return;
+void compute_gate(Circuit& circuit, ParityAnd& gate, std::vector<WireRef> const& to_qubit, WireRef target)
+{
+	std::vector<WireRef> in0;
+	std::vector<WireRef> in1;
+	std::vector<WireRef> in01;
+	for (uint32_t i : gate.in0) {
+		in0.push_back(to_qubit.at(i));
+	}
+	for (uint32_t i : gate.in1) {
+		in1.push_back(to_qubit.at(i));
+	}
+	for (uint32_t i : gate.in01) {
+		in01.push_back(to_qubit.at(i));
+	}
+	compute_parity(circuit, in0);
+	if (!in01.empty()) {
+		compute_parity(circuit, in01);
+		in1.push_back(in01.back());
+		circuit.create_instruction(GateLib::X(), {in01.back()}, in0.back());
+	}
+	compute_parity(circuit, in1);
+	// Compute Toffoli
+	WireRef c0 = gate.is_complemented[0] ? !in0.back() : in0.back();
+	WireRef c1 = gate.is_complemented[1] ? !in1.back() : in1.back();
+	circuit.create_instruction(GateLib::X(), {c0, c1}, target);
+	compute_parity(circuit, in1);
+	if (!in01.empty()) {
+		circuit.create_instruction(GateLib::X(), {in01.back()}, in0.back());
+		compute_parity(circuit, in01);
+	}
+	compute_parity(circuit, in0);
+		
+}
+
+void synthesize(Circuit& circuit, std::vector<WireRef> const& qubits,
+    mockturtle::xag_network const& xag)
+{
+	std::vector<ParityAnd> gates = collapse_xag(xag);
+	std::vector<Step> steps;
+	for (uint32_t i = xag.num_pis() + 1; i < gates.size() - xag.num_pos(); ++i) {
+		if (gates[i].in0.size() < gates[i].in1.size()) {
+			std::swap(gates[i].in0, gates[i].in1);
+			std::swap(gates[i].is_complemented[0], gates[i].is_complemented[1]);
 		}
-		if (xag.visited(node)) {
-			return;
+		steps.emplace_back(Action::compute, i);
+		for (uint32_t id : gates[i].in0) {
+			gates[id].ref_count -= 1;
+			if (gates[id].ref_count == 0 && id >= xag.num_pis() + 1) {
+				steps.emplace_back(Action::cleanup, id);
+			}
 		}
-		xag.set_visited(node, 1);
-		std::vector<WireRef> qubits;
-		for (XAGSignal const& in : state.ltfi[node]) {
-			if (state.to_qubit[in] == state.to_qubit[signal]) {
+		for (uint32_t id : gates[i].in1) {
+			gates[id].ref_count -= 1;
+			if (gates[id].ref_count == 0 && id >= xag.num_pis() + 1) {
+				steps.emplace_back(Action::cleanup, id);
+			}
+		}
+		for (uint32_t id : gates[i].in01) {
+			gates[id].ref_count -= 1;
+			if (gates[id].ref_count == 0 && id >= xag.num_pis() + 1) {
+				steps.emplace_back(Action::cleanup, id);
+			}
+		}
+	}
+
+	// Assing qubits to the Inputs
+	std::vector<WireRef> to_qubit(gates.size(), WireRef::invalid());
+	for (uint32_t i = 0; i < xag.num_pis(); ++i) {
+		to_qubit[i + 1] = qubits.at(i);
+	}
+	// Assing qubits to the Outputs
+	uint32_t o = xag.num_pis();
+	// for (uint32_t i = gates.size(); i --> gates.size() - xag.num_pos();) {
+	for (uint32_t i = gates.size() - xag.num_pos(); i < gates.size(); ++i) {
+		for (uint32_t id : gates[i].in0) {
+			if (to_qubit[id] == WireRef::invalid()) {
+				if (gates[id].ref_count == 1 && id >= xag.num_pis() + 1) {
+					// gates[id].ref_count -= 1;
+				// if (i >= xag.num_pis() + 1) {
+					to_qubit[id] = qubits.at(o);
+					break;
+				}
+			}
+		}
+		o += 1;
+	}
+
+	for (Step const& step : steps) {
+		ParityAnd& gate = gates[step.node];
+		switch (step.action) {
+		case Action::compute:
+			if (to_qubit[step.node] == WireRef::invalid()) {
+				to_qubit[step.node] = circuit.request_ancilla();
+			}
+			compute_gate(circuit, gate, to_qubit, to_qubit[step.node]);
+			break;
+
+		case Action::cleanup:
+			circuit.release_ancilla(to_qubit[step.node]);
+			compute_gate(circuit, gate, to_qubit, to_qubit[step.node]);
+			break;
+		}
+	}
+	// o = xag.num_pis();
+	o = qubits.size() - 1;
+	for (uint32_t i = gates.size(); i --> gates.size() - xag.num_pos();) {
+	// for (uint32_t i = gates.size() - xag.num_pos(); i < gates.size(); ++i) {
+		std::vector<WireRef> qs;
+		for (uint32_t id : gates[i].in0) {
+			if (to_qubit[id] == qubits.at(o)) {
 				continue;
 			}
-			qubits.push_back(state.to_qubit[in]);
+			gates[id].ref_count -= 1;
+			qs.push_back(to_qubit[id]);
 		}
-		qubits.push_back(state.to_qubit[signal]);
-		compute_parity(circuit, qubits);
-	});
-	uint32_t index = xag.num_pis();
-	xag.foreach_po([&](XAGSignal const& signal) {
-		XAGNode const& node = xag.get_node(signal);
-		if (xag.is_constant(node)) {
-			index += 1;
-			return;
+		qs.push_back(qubits.at(o));
+		compute_parity(circuit, qs);
+		for (uint32_t id : gates[i].in0) {
+			if (gates[id].ref_count == 0 && id >= xag.num_pis() + 1) {
+				compute_gate(circuit, gates[id], to_qubit, to_qubit[id]);
+			};
 		}
-		if (xag.is_pi(node)) {
-			circuit.create_instruction(GateLib::X(), {state.to_qubit[signal]}, qubits[index]);
-			index += 1;
-			return;
+		o -= 1;
+	}
+	o = xag.num_pis();
+	for (uint32_t i = gates.size() - xag.num_pos(); i < gates.size(); ++i) {
+		if (gates[i].is_complemented[0]) {
+			circuit.create_instruction(GateLib::X(), {qubits.at(o)});
 		}
-		if (xag.visited(node) == 2) {
-			circuit.create_instruction(GateLib::X(), {state.to_qubit[signal]}, qubits[index]);
-		}
-		xag.set_visited(node, 2);
-		index += 1;
-	});
-	index = xag.num_pis();
-	xag.foreach_po([&](XAGSignal const& signal) {
-		if (xag.is_complemented(signal)) {
-			circuit.create_instruction(GateLib::X(), {qubits[index]});
-		}
-		index += 1;
-	});
-	// // Cleanup
-	// auto begin = gates.crbegin();
-	// auto end = gates.crend();
-	// for (;begin != end; ++begin) {
-	// 	if (begin->cleanup == false) {
-	// 		continue;
-	// 	}
-	// 	compute_parity(circuit, begin->in1);
-	// 	if (!begin->in01.empty()) {
-	// 		compute_parity(circuit, begin->in01);
-	// 		circuit.create_instruction(GateLib::X(), {begin->in01.back()}, begin->in0.back());
-	// 	}
-	// 	compute_parity(circuit, begin->in0);
-	// 	WireRef c0 = begin->is_complemented[0] ? !begin->in0.back() : begin->in0.back();
-	// 	WireRef c1 = begin->is_complemented[1] ? !begin->in1.back() : begin->in1.back();
-	// 	circuit.create_instruction(GateLib::X(), {c0, c1}, begin->target);
-	// 	compute_parity(circuit, begin->in1);
-	// 	if (!begin->in01.empty()) {
-	// 		circuit.create_instruction(GateLib::X(), {begin->in01.back()}, begin->in0.back()); 
-	// 		compute_parity(circuit, begin->in01);
-	// 	}
-	// 	compute_parity(circuit, begin->in0);
-	// }
+		o += 1;
+	}
 }
+
 } // namespace xag_synth_detail
 #pragma endregion
 
