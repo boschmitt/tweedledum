e404b55b608ee0f1a3d4717933d11531b1ce1fb8
Bruno Schmitt <bruno.schmitt@epfl.ch>
2021-02-15 15:12:55 -0300

    Missing comma :(

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9"
     ]
 )
