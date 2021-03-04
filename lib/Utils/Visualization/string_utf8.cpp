6b74154c6cdb73b18a2cb9d212eed9368b73ad8a
Bruno Schmitt <bruno.schmitt@epfl.ch>
2021-03-04 22:01:38 +0100

    Second refactor of the utf8 printer

--- lib/Utils/Visualization/string_utf8.cpp
+++ lib/Utils/Visualization/string_utf8.cpp
@@ -2,6 +2,7 @@
 | Part of Tweedledum Project.  This file is distributed under the MIT License.
 | See accompanying file /LICENSE for details.
 *-----------------------------------------------------------------------------*/
+#include "tweedledum/Operators/Standard/Swap.h"
 #include "tweedledum/Utils/Visualization/string_utf8.h"
 
 #include <algorithm>
@@ -10,19 +11,6 @@
 #include <utility>
 
 namespace tweedledum {
-
-// Width is given by the label!
-struct Box {
-    uint32_t left_x;
-    uint32_t right_x;
-    uint32_t top_y;
-    uint32_t bot_y;
-    bool overlap;
-    std::string_view label;
-    std::vector<uint32_t> targets;
-    std::vector<std::pair<uint32_t, uint32_t>> controls;
-    std::vector<WireRef> cbits;
-};
 
 inline void merge_chars(char32_t& c0, char32_t c1)
 { 
@@ -72,13 +60,13 @@ inline void merge_chars(char32_t& c0, char32_t c1)
     }
     if (c0 == U'─') {
         switch (c1) {
-        case U'┌':
-        case U'┐':
+        case U'╭':
+        case U'╮':
             c0 = U'┬';
             return;
 
-        case U'└':
-        case U'┘':
+        case U'╰':
+        case U'╯':
             c0 = U'┴';
             return;
 
@@ -86,20 +74,188 @@ inline void merge_chars(char32_t& c0, char32_t c1)
             break;
         }
     }
-    if (c0 == U'┌' && c1 == U'└') {
+    if (c0 == U'╭' && c1 == U'╰') {
         c0 = U'├';
         return;
-    } else if (c0 == U'┐' && c1 == U'┘') {
+    } else if (c0 == U'╮' && c1 == U'╯') {
         c0 = U'┤';
         return;
     }
 }
 
-std::string to_string_utf8(Circuit const& circuit)
+struct DiagramShape {
+    uint32_t left_col;
+    uint32_t right_col;
+    uint32_t top_row;
+    uint32_t bot_row;
+    std::string_view label;
+    std::vector<uint32_t> target_rows;
+    std::vector<uint32_t> control_rows;
+    std::vector<WireRef> cbits;
+
+    virtual uint32_t width() const = 0;
+    virtual void draw(std::vector<std::u32string>& lines) const = 0;
+
+    void draw_cbits(std::vector<std::u32string>& lines) const
+    {
+        uint32_t const mid_col = (left_col + right_col) / 2;
+        uint32_t const cbit_line = lines.size() - 2;
+        for (WireRef const wire : cbits) {
+            std::u32string const wire_label = fmt::format(U"{:>2}", wire.uid());
+            lines.at(cbit_line).at(mid_col) = wire.is_complemented() ? U'◯' : U'●';
+            for (uint32_t i = bot_row + 1; i < cbit_line; ++i) {
+                merge_chars(lines.at(i).at(mid_col), U'║');
+            }
+            lines.at(bot_row).at(mid_col) = U'╥';
+            std::copy(wire_label.begin(), wire_label.end(), 
+                      lines.at(cbit_line + 1).begin() + (mid_col - 1));
+        }
+    }
+};
+
+struct Box : public DiagramShape {
+    virtual uint32_t width() const override
+    {
+        // The width is the label legnth + two box chars + two spaces + 
+        return label.size() + 4 + (!control_rows.empty());
+    }
+
+    virtual void draw(std::vector<std::u32string>& lines) const override
+    {
+        draw_box(lines);
+        draw_targets(lines);
+        draw_controls(lines);
+        if (!cbits.empty()) {
+            draw_cbits(lines);
+        }
+        draw_label(lines);
+    }
+
+    void draw_box(std::vector<std::u32string>& lines) const
+    {
+        for (uint32_t i = left_col + 1; i < right_col; ++i) {
+            lines.at(top_row).at(i) = U'─';
+            lines.at(bot_row).at(i) = U'─';
+        }
+        for (uint32_t i = top_row + 1; i < bot_row; ++i) {
+            auto left = lines.at(i).begin() + left_col;
+            auto right = lines.at(i).begin() + right_col;
+            *left = U'│';
+            *right = U'│';
+            std::fill(++left, right, U' ');
+        }
+        merge_chars(lines.at(top_row).at(left_col), U'╭');
+        merge_chars(lines.at(bot_row).at(left_col), U'╰');
+        merge_chars(lines.at(top_row).at(right_col), U'╮');
+        merge_chars(lines.at(bot_row).at(right_col), U'╯');
+    }
+
+    void draw_targets(std::vector<std::u32string>& lines) const
+    {
+        for (uint32_t const line : target_rows) {
+            lines.at(line).at(left_col) = U'┤';
+            lines.at(line).at(right_col) = U'├';
+        }
+    }
+
+    virtual void draw_controls(std::vector<std::u32string>& lines) const
+    {
+        for (uint32_t const control : control_rows) {
+            uint32_t line = (control >> 1);
+            lines.at(line).at(left_col) = U'┤';
+            lines.at(line).at(left_col + 1) = control & 1 ? U'◯' : U'●';
+            lines.at(line).at(right_col) = U'├';
+        }
+    }
+
+    virtual void draw_label(std::vector<std::u32string>& lines) const
+    {
+        // Draw label
+        uint32_t const mid_row = (top_row + bot_row)/2;
+        std::copy(label.begin(), label.end(), 
+                  lines.at(mid_row).begin() + left_col + 2 + (!control_rows.empty()));
+    }
+};
+
+struct ControlledBox : public Box {
+    virtual uint32_t width() const override
+    {
+        // The width is the label legnth + two box chars + two spaces + 
+        return label.size() + 4;
+    }
+
+    virtual void draw_controls(std::vector<std::u32string>& lines) const override
+    {
+        uint32_t const mid_col = (left_col + right_col) / 2;
+        for (uint32_t const control: control_rows) {
+            uint32_t const line = (control >> 1);
+            lines.at(line).at(mid_col) = control & 1 ? U'◯' : U'●';
+            if (line < top_row) {
+                for (uint32_t i = line + 1; i < top_row; ++i) {
+                    merge_chars(lines.at(i).at(mid_col), U'│');
+                }
+                lines.at(top_row).at(mid_col) = U'┴';
+            } else {
+                for (uint32_t i = top_row + 1; i < line; ++i) {
+                    merge_chars(lines.at(i).at(mid_col), U'│');
+                }
+                lines.at(bot_row).at(mid_col) = U'┬';
+            }
+        }
+    }
+
+    virtual void draw_label(std::vector<std::u32string>& lines) const override
+    {
+        // Draw label
+        uint32_t const mid_row = (top_row + bot_row)/2;
+        std::copy(label.begin(), label.end(),
+                  lines.at(mid_row).begin() + left_col + 2);
+    }
+};
+
+struct DiagramSwap : public DiagramShape {
+    virtual uint32_t width() const override
+    {
+        return 3u;
+    }
+
+    virtual void draw(std::vector<std::u32string>& lines) const override
+    {
+        uint32_t const mid_col = left_col + 1;
+        lines.at(top_row + 1).at(mid_col) = U'╳';
+        for (uint32_t i = top_row + 2; i < bot_row - 1; ++i) {
+            merge_chars(lines.at(i).at(mid_col), U'│');
+        }
+        lines.at(bot_row - 1).at(mid_col) = U'╳';
+        draw_controls(lines);
+        if (!cbits.empty()) {
+            draw_cbits(lines);
+        }
+    }
+
+    void draw_controls(std::vector<std::u32string>& lines) const
+    {
+        uint32_t const mid_col = left_col + 1;
+        for (uint32_t const control : control_rows) {
+            uint32_t const line = (control >> 1);
+            lines.at(line).at(mid_col) = control & 1 ? U'◯' : U'●';
+            if (line <= top_row) {
+                for (uint32_t i = line + 1; i <= top_row; ++i) {
+                    merge_chars(lines.at(i).at(mid_col), U'│');
+                }
+            } else {
+                for (uint32_t i = bot_row; i < line; ++i) {
+                    merge_chars(lines.at(i).at(mid_col), U'│');
+                }
+            }
+        }
+    }
+};
+
+
+std::string to_string_utf8(Circuit const& circuit, uint32_t const max_rows)
 {
-    constexpr uint32_t padding = 4u;
     using Layer = std::vector<InstRef>;
-    
     if (circuit.num_wires() == 0) {
         return "";
     }
@@ -107,52 +263,72 @@ std::string to_string_utf8(Circuit const& circuit)
     uint32_t const num_dwire = circuit.num_qubits() + (circuit.num_cbits() > 0);
     std::vector<uint32_t> wire_to_dwire(circuit.num_wires(), num_dwire - 1);
     uint32_t curr_dwire = 0;
-    circuit.foreach_wire([&](WireRef wire) {
-        if (wire.kind() == Wire::Kind::classical) {
+    std::size_t prefix_size = 0;
+    std::vector<std::string> prefix((2 * num_dwire) + 1, "");
+    circuit.foreach_wire([&](WireRef wref, Wire const& wire) {
+        if (wref.kind() == Wire::Kind::classical) {
             return;
         }
-        wire_to_dwire.at(wire) = curr_dwire++;
+        wire_to_dwire.at(wref) = curr_dwire;
+        prefix.at((2 * curr_dwire++) + 1) = wire.name + " : ";
+        prefix_size = std::max(prefix_size, wire.name.size() + 3);
     });
     // Separete the intructions in layers.  Each layer must contain gates that
     // can be drawn in the same diagram layer.  For example, if I have a
     // CX(0, 2) and a X(1), then I cannot draw those gates on the same layer
     // in the circuit diagram
-    std::vector<Box> boxes(circuit.size());
+    std::vector<std::unique_ptr<DiagramShape>> boxes;
     std::vector<Layer> layers;
     std::vector<uint32_t> layer_width;
     std::vector<int> wire_layer(num_dwire, -1);
     circuit.foreach_instruction([&](InstRef ref, Instruction const& inst) {
-        Box& box = boxes.at(ref);
-        box.label = inst.name();
+        std::vector<uint32_t> target_rows;
         inst.foreach_target([&](WireRef wire) {
-            uint32_t const dwire = wire_to_dwire.at(wire);
-            box.targets.push_back(dwire);
+            target_rows.push_back((2 * wire_to_dwire.at(wire)) + 1);
         });
-        std::sort(box.targets.begin(), box.targets.end());
-        uint32_t const min_target = box.targets.front();
-        uint32_t const max_target = box.targets.back();
+        std::sort(target_rows.begin(), target_rows.end());
+        uint32_t const min_target = ((target_rows.front() - 1) >> 1);
+        uint32_t const max_target = ((target_rows.back() - 1) >> 1);
         uint32_t min = min_target;
         uint32_t max = max_target;
-        box.overlap = (inst.num_cbits() > 0) && (inst.num_controls() > 0);
+
+        bool overlap = (inst.num_cbits() > 0) && (inst.num_controls() > 0);
+        std::vector<uint32_t> control_rows;
         inst.foreach_control([&](WireRef wire) {
-            auto const [control, _] = box.controls.emplace_back(wire_to_dwire.at(wire), wire.is_complemented());
-            if (control > min_target && control < max_target) {
-                box.overlap = true;
+            uint32_t const dwire = wire_to_dwire.at(wire);
+            uint32_t const line = (2 * wire_to_dwire.at(wire)) + 1;
+            control_rows.push_back((line << 1) | wire.is_complemented());
+            if (dwire > min_target && dwire < max_target) {
+                overlap = true;
             }
-            if (control > max) {
-                max = control;
-            } else if (control < min) {
-                min = control;
+            if (dwire > max) {
+                max = dwire;
+            } else if (dwire < min) {
+                min = dwire;
             }
         });
+        std::vector<WireRef> cbits;
         inst.foreach_cbit([&](WireRef wire) {
-            box.cbits.push_back(wire);
+            cbits.push_back(wire);
         });
-        box.top_y = 2 * (min_target);
-        box.bot_y = 2 * (max_target) + 2;
-        if (box.overlap) {
-            box.top_y = 2 * (min);
-            box.bot_y = 2 * (max) + 2;
+
+        std::unique_ptr<DiagramShape> shape = nullptr;
+        if (overlap) {
+            shape = std::make_unique<Box>();
+        } else if (inst.is_a<Op::Swap>()) {
+            shape = std::make_unique<DiagramSwap>();
+        } else {
+            shape = std::make_unique<ControlledBox>();
+        }
+        shape->label = inst.name();
+        shape->cbits = cbits;
+        shape->control_rows = control_rows;
+        shape->target_rows = target_rows;
+        shape->top_row = (2 * min_target);
+        shape->bot_row = (2 * max_target) + 2;
+        if (overlap) {
+            shape->top_row = (2 * min);
+            shape->bot_row = (2 * max ) + 2;
         }
 
         int layer = -1;
@@ -168,21 +344,28 @@ std::string to_string_utf8(Circuit const& circuit)
         for (uint32_t i = min; i <= max; ++i) {
             wire_layer.at(i) = layer;
         }
-        uint32_t const width = inst.name().size() + padding + (2 * box.overlap);
-        layer_width.at(layer) = std::max(layer_width.at(layer), width);
-        // TODO: Edge cases: Swap, Measure
+        layer_width.at(layer) = std::max(layer_width.at(layer), shape->width());
+        boxes.push_back(std::move(shape));
     });
 
     // Compute diagram width
     uint32_t curr_width = 0u;
+    uint32_t acc_width = prefix_size;
+    std::vector<uint32_t> cutting_point;
     for (uint32_t layer = 0u; layer < layers.size(); ++layer) {
         for (InstRef const ref : layers.at(layer)) {
-            Box& box = boxes.at(ref);
-            box.left_x = curr_width + (layer_width.at(layer) - (box.label.size() + padding))/2u;
-            box.right_x = box.left_x + box.label.size() + padding + box.overlap - 1;
+            auto const& box = boxes.at(ref);
+            box->left_col = curr_width + (layer_width.at(layer) - box->width())/2u;
+            box->right_col = box->left_col + box->width() - 1;
+        }
+        if ((acc_width + layer_width.at(layer)) >= (max_rows - 1)) {
+            cutting_point.push_back(curr_width);
+            acc_width = 0u;
         }
         curr_width += layer_width.at(layer);
+        acc_width += layer_width.at(layer);
     }
+    cutting_point.push_back(curr_width);
 
     // Initialize the diagram with the wires 
     uint32_t const num_lines = (2 * num_dwire) + 1;
@@ -200,86 +383,39 @@ std::string to_string_utf8(Circuit const& circuit)
     }
 
     // Draw boxes
-    for (Box const& box : boxes) {
-        // Draw box
-        for (uint32_t i = box.left_x + 1; i < box.right_x; ++i) {
-            lines.at(box.top_y).at(i) = U'─';
-            lines.at(box.bot_y).at(i) = U'─';
-        }
-        for (uint32_t i = box.top_y + 1; i < box.bot_y; ++i) {
-            auto left = lines.at(i).begin() + box.left_x;
-            auto right = lines.at(i).begin() + box.right_x;
-            *left = U'│';
-            *right = U'│';
-            std::fill(++left, right, U' ');
-        }
-        merge_chars(lines.at(box.top_y).at(box.left_x), U'┌');
-        merge_chars(lines.at(box.bot_y).at(box.left_x), U'└');
-        merge_chars(lines.at(box.top_y).at(box.right_x), U'┐');
-        merge_chars(lines.at(box.bot_y).at(box.right_x), U'┘');
-        // Connect targets
-        for (auto const wire : box.targets) {
-            uint32_t const line = (2u * wire) + 1;
-            lines.at(line).at(box.left_x) = U'┤';
-            lines.at(line).at(box.right_x) = U'├';
-        }
-        // Connect controls
-        uint32_t const mid_x = box.left_x + ((box.label.size() + padding) / 2u);
-        if (box.overlap) {
-            for (auto const& [wire, is_complemented] : box.controls) {
-                uint32_t const line = (2u * wire) + 1;
-                lines.at(line).at(box.left_x) = U'┤';
-                lines.at(line).at(box.left_x + 1) = is_complemented ? U'◯' : U'●';
-                lines.at(line).at(box.right_x) = U'├';
-            }
-        } else {
-            for (auto const& [wire, is_complemented] : box.controls) {
-                uint32_t const line = (2u * wire) + 1;
-                lines.at(line).at(mid_x) = is_complemented ? U'◯' : U'●';
-                if (line < box.top_y) {
-                    for (uint32_t i = line + 1; i < box.top_y; ++i) {
-                        merge_chars(lines.at(i).at(mid_x), U'│');
-                    }
-                    lines.at(box.top_y).at(mid_x) = U'┴';
-                } else {
-                    for (uint32_t i = box.top_y + 1; i < line; ++i) {
-                        merge_chars(lines.at(i).at(mid_x), U'│');
-                    }
-                    lines.at(box.bot_y).at(mid_x) = U'┬';
-                }
-            }
-        }
-        // Connect classical bits
-        uint32_t const cbit_line = (2u * (num_dwire - 1) + 1);
-        for (WireRef const wire : box.cbits) {
-            std::u32string const wire_label = fmt::format(U"{:>2}", wire.uid());
-            lines.at(cbit_line).at(mid_x) = wire.is_complemented() ? U'◯' : U'●';
-            for (uint32_t i = box.bot_y + 1; i < cbit_line; ++i) {
-                merge_chars(lines.at(i).at(mid_x), U'║');
-            }
-            lines.at(box.bot_y).at(mid_x) = U'╥';
-            std::copy(wire_label.begin(), wire_label.end(), 
-                  lines.at(cbit_line + 1).begin() + (mid_x - 1));
-        }
-        
-        // Draw label
-        uint32_t const mid_y = (box.top_y + box.bot_y)/2;
-        std::copy(box.label.begin(), box.label.end(), 
-                  lines.at(mid_y).begin() + box.left_x + 2 + box.overlap);
+    for (auto const& box : boxes) {
+        box->draw(lines);
     }
 
     std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
     std::string str;
     str.reserve(curr_width * num_lines * 4);
-    for (auto const& line : lines) {
-        str += conv.to_bytes(line) + '\n';
+
+    uint32_t start = 0;
+    for (uint32_t i = 0; i < cutting_point.size(); ++i) {
+        if (i > 0) {
+            str += fmt::format("\n{:#^{}}\n\n", "", max_rows);
+        }
+        for (uint32_t line = 0; line < lines.size(); ++line) {
+            auto being = lines.at(line).data() + start;
+            auto end = lines.at(line).data() + cutting_point.at(i);
+            if (i == 0) {
+                str += fmt::format("{: >{}}", prefix.at(line), prefix_size);
+            }
+            str += conv.to_bytes(being, end);
+            if (i + 1 < cutting_point.size()) {
+                str += conv.to_bytes(U'»');
+            }
+            str += '\n';
+        }
+        start = cutting_point.at(i);
     }
     return str;
 }
 
-void print(Circuit const& circuit)
+void print(Circuit const& circuit, uint32_t const max_rows)
 {
-    fmt::print("{}", to_string_utf8(circuit));
+    fmt::print("{}", to_string_utf8(circuit, max_rows));
 }
 
 } // namespace tweedledum
