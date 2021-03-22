bc786d5ead7f08d02e0bc730468b14688a612d92
Bruno Schmitt <bruno.schmitt@epfl.ch>
2021-03-22 20:29:30 +0100

    Swap circuit generation based on A* search

---
+++ include/tweedledum/Synthesis/a_star_swap_synth.h
@@ -0,0 +1,20 @@
+/*------------------------------------------------------------------------------
+| Part of Tweedledum Project.  This file is distributed under the MIT License.
+| See accompanying file /LICENSE for details.
+*-----------------------------------------------------------------------------*/
+#pragma once
+
+#include "../IR/Circuit.h"
+#include "../Target/Device.h"
+
+#include <nlohmann/json.hpp>
+#include <vector>
+
+namespace tweedledum {
+
+Circuit a_star_swap_synth(Device const& device, 
+    std::vector<uint32_t> const& init_cfg,
+    std::vector<uint32_t> const& final_cfg,
+    nlohmann::json const& config = {});
+
+} // namespace tweedledum
