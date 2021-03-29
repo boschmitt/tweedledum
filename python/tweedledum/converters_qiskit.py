75bbcdfca14d1a8120c2b0a6313bd343660f35be
Bruno Schmitt <bruno.schmitt@epfl.ch>
2021-03-29 10:59:21 +0200

    Handle measurment when converting to/from qiskit

--- python/tweedledum/converters_qiskit.py
+++ python/tweedledum/converters_qiskit.py
@@ -3,37 +3,39 @@
 # See accompanying file /LICENSE for details.
 #-------------------------------------------------------------------------------
 from tweedledum.ir import Circuit, Qubit, Cbit, rotation_angle
-from tweedledum import operators as dum_ops
+from tweedledum import operators as Op
 
-from qiskit import QuantumRegister
-from qiskit.circuit import QuantumCircuit, Gate, ControlledGate
+from qiskit.circuit import (QuantumCircuit, QuantumRegister, Gate, 
+    ControlledGate, Measure)
 from qiskit.dagcircuit.dagcircuit import DAGCircuit
 from qiskit.circuit.library.standard_gates import (HGate, IGate, PhaseGate, 
     RXGate, RYGate, RZGate, RXXGate, RYYGate, RZZGate, SGate, SdgGate, SwapGate,
     TGate, TdgGate, XGate, YGate, ZGate)
 
-_to_tweedledum_op = {
-    'h': dum_ops.H, 
-    'p': dum_ops.P,
-    'rx': dum_ops.Rx,
-    'ry': dum_ops.Ry,
-    'rz': dum_ops.Rz,
-    'rxx': dum_ops.Rxx,
-    'ryy': dum_ops.Ryy,
-    'rzz': dum_ops.Rzz,
-    's': dum_ops.S,
-    'sdg': dum_ops.Sdg, 
-    'swap': dum_ops.Swap,
-    't': dum_ops.T,
-    'tdg': dum_ops.Tdg, 
-    'x': dum_ops.X,
-    'y': dum_ops.Y,
-    'z': dum_ops.Z
+_TO_TWEEDLEDUM_OP = {
+    'h': Op.H, 
+    'p': Op.P,
+    'rx': Op.Rx,
+    'ry': Op.Ry,
+    'rz': Op.Rz,
+    'rxx': Op.Rxx,
+    'ryy': Op.Ryy,
+    'rzz': Op.Rzz,
+    's': Op.S,
+    'sdg': Op.Sdg, 
+    'swap': Op.Swap,
+    't': Op.T,
+    'tdg': Op.Tdg, 
+    'x': Op.X,
+    'y': Op.Y,
+    'z': Op.Z,
+    'measure': Op.Measure
 }
 
-_to_qiskit_gate = {
+_TO_QISKIT_GATE = {
     'std.h': HGate,
     'std.p': PhaseGate,
+    'std.m': Measure,
     'std.rx': RXGate,
     'std.ry': RYGate,
     'std.rz': RZGate,
@@ -51,7 +53,7 @@ _to_qiskit_gate = {
 }
 
 def _convert_qiskit_operator(gate):
-    op = _to_tweedledum_op.get(gate.name) or _to_tweedledum_op.get(gate.base_gate.name)
+    op = _TO_TWEEDLEDUM_OP.get(gate.name) or _TO_TWEEDLEDUM_OP.get(gate.base_gate.name)
     ctrl_state = ''
     if isinstance(gate, ControlledGate):
         ctrl_state = '{:0{}b}'.format(gate.ctrl_state, gate.num_ctrl_qubits)[::-1]
@@ -68,32 +70,38 @@ def qiskit_qc_to_tweedledum(qiskit_qc):
     circuit = Circuit()
     qubits = [circuit.create_qubit() for i in range(len(qiskit_qc.qubits))]
     qubits_map = dict(zip(qiskit_qc.qubits, qubits))
+    cbits = [circuit.create_cbit() for i in range(len(qiskit_qc.clbits))]
+    cbits_map = dict(zip(qiskit_qc.clbits, cbits))
 
-    for instruction, qargs, _ in qiskit_qc.data:
+    for instruction, qargs, cargs in qiskit_qc.data:
         op, ctrl_state = _convert_qiskit_operator(instruction)
         qs = [qubits_map.get(qubit) for qubit in qargs]
+        cs = [cbits_map.get(cbit) for cbit in cargs]
         for i, polarity in enumerate(ctrl_state):
             if polarity == '0':
                 qs[i] = ~qs[i]
-        circuit.apply_operator(op, qs)
+        circuit.apply_operator(op, qs, cs)
     return circuit
 
 def qiskit_dag_to_tweedledum(qiskit_dag):
     circuit = Circuit()
     qubits = [circuit.create_qubit() for i in range(len(qiskit_dag.qubits))]
     qubits_map = dict(zip(qiskit_dag.qubits, qubits))
+    cbits = [circuit.create_cbit() for i in range(len(qiskit_qc.clbits))]
+    cbits_map = dict(zip(qiskit_qc.clbits, cbits))
 
     for node in qiskit_dag.op_nodes():
         op, ctrl_state = _convert_qiskit_operator(node.op)
         qs = [qubits_map.get(qubit) for qubit in node.qargs]
+        cs = [cbits_map.get(cbit) for cbit in node.cargs]
         for i, polarity in enumerate(ctrl_state):
             if polarity == '0':
                 qs[i] = ~qs[i]
-        circuit.apply_operator(op, qs)
+        circuit.apply_operator(op, qs, cs)
     return circuit
 
 def _convert_tweedledum_op(op):
-    base_gate = _to_qiskit_gate.get(op.kind())
+    base_gate = _TO_QISKIT_GATE.get(op.kind())
     if base_gate == None:
         if op.kind() == 'py_operator':
             return op.py_op()
@@ -117,18 +125,26 @@ def _convert_tweedledum_op(op):
     return gate
 
 def tweedledum_to_qiskit_qc(circuit):
-    qiskit_qc = QuantumCircuit(circuit.num_qubits())
+    qiskit_qc = QuantumCircuit()
+    # Qiskit is weird, it doesn't allow passing 0 as size for register.
+    if circuit.num_cbits():
+        qiskit_qc.add_register(circuit.num_qubits(), circuit.num_cbits())
+    else:
+        qiskit_qc.add_register(circuit.num_qubits()) 
+
     for instruction in circuit:
         gate = _convert_tweedledum_op(instruction)
         qubits = [qubit.uid() for qubit in instruction.qubits()]
-        qiskit_qc.append(gate, qubits)
+        cbits = [cbit.uid() for cbit in instruction.cbits()]
+        qiskit_qc.append(gate, qubits, cbits)
     return qiskit_qc
 
 def tweedledum_to_qiskit_dag(circuit):
     qiskit_dag = DAGCircuit()
-    qiskit_dag.add_qreg(circuit.num_qubits())
+    qiskit_dag.add_qreg(circuit.num_qubits(), circuit.num_cbits())
     for instruction in circuit:
         gate = _convert_tweedledum_op(instruction)
         qubits = [qubit.uid() for qubit in instruction.qubits()]
-        qiskit_dag.apply_operation_back(gate, qubits)
+        cbits = [cbit.uid() for cbit in instruction.cbits()]
+        qiskit_dag.apply_operation_back(gate, qubits, cbits)
     return qiskit_dag
