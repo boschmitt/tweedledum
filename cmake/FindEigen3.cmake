0a01475057aeab5e7ca9ceed838719e51769e326
Bruno Schmitt <bruno.schmitt@epfl.ch>
2021-04-23 15:07:55 +0200

    Initial refactoring of the building system

---
+++ cmake/FindEigen3.cmake
@@ -0,0 +1,92 @@
+# Distributed under the MIT License (See accompanying file /LICENSE)
+#[=======================================================================[.rst:
+FindEigen3
+-------
+
+Finds the Eigen3 library.
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+This module provides the following imported targets, if found:
+
+``Eigen3::Eigen3``
+  The Eigen3 library
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``Eigen3_FOUND``
+  True if the system has the Eigen3 library.
+``Eigen3_VERSION``
+  The version of the Eigen3 library which was found.
+``Eigen3_INCLUDE_DIRS``
+  Include directories needed to use Eigen3.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``Eigen3_INCLUDE_DIR``
+  The directory containing ``signature_of_eigen3_matrix_library``.
+
+#]=======================================================================]
+find_package(PkgConfig)
+pkg_check_modules(PC_Eigen3 QUIET Eigen3)
+
+find_path(Eigen3_INCLUDE_DIR
+    NAMES Eigen/signature_of_eigen3_matrix_library
+    PATHS
+        ${PC_Eigen3_INCLUDE_DIRS}
+        ${CMAKE_INSTALL_PREFIX}/include
+        ${PROJECT_SOURCE_DIR}/third_party
+    PATH_SUFFIXES 
+        eigen3 eigen
+)
+
+macro(_Eigen3_get_version)
+    file(READ "${Eigen3_INCLUDE_DIR}/Eigen/src/Core/util/Macros.h" _header)
+    string(CONCAT VERSION_REGEX
+        "#define[ \t]+EIGEN_WORLD_VERSION[ \t]+([0-9]+)[ \n]+"
+        "#define[ \t]+EIGEN_MAJOR_VERSION[ \t]+([0-9]+)[ \n]+"
+        "#define[ \t]+EIGEN_MINOR_VERSION[ \t]+([0-9]+)")
+    string(REGEX MATCH ${VERSION_REGEX} _ "${_header}")
+    # Use math to skip leading zeros if any.
+    math(EXPR Eigen3_WORLD_VERSION ${CMAKE_MATCH_1})
+    math(EXPR Eigen3_MAJOR_VERSION ${CMAKE_MATCH_2})
+    math(EXPR Eigen3_MINOR_VERSION ${CMAKE_MATCH_3})
+    string(JOIN "." Eigen3_VERSION ${Eigen3_WORLD_VERSION}
+                                   ${Eigen3_MAJOR_VERSION}
+                                   ${Eigen3_MINOR_VERSION})
+endmacro()
+
+set(Eigen3_VERSION ${PC_Eigen3_VERSION})
+if (NOT Eigen3_VERSION)
+    _Eigen3_get_version()
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Eigen3
+    FOUND_VAR Eigen3_FOUND
+    REQUIRED_VARS
+        Eigen3_INCLUDE_DIR
+    VERSION_VAR Eigen3_VERSION
+)
+
+if(Eigen3_FOUND)
+    set(Eigen3_INCLUDE_DIRS ${Eigen3_INCLUDE_DIR})
+    set(Eigen3_DEFINITIONS ${PC_Eigen3_CFLAGS_OTHER})
+endif()
+
+if(Eigen3_FOUND AND NOT TARGET Eigen3::Eigen3)
+    add_library(Eigen3::Eigen3 INTERFACE IMPORTED)
+    set_target_properties(Eigen3::Eigen3 PROPERTIES
+        INTERFACE_COMPILE_OPTIONS "${PC_Eigen3_CFLAGS_OTHER}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Eigen3_INCLUDE_DIR}"
+    )
+endif()
+
+mark_as_advanced(Eigen3_INCLUDE_DIR)
---
+++ cmake/FindPhmap.cmake
@@ -0,0 +1,92 @@
+# Distributed under the MIT License (See accompanying file /LICENSE)
+#[=======================================================================[.rst:
+Find phmap
+-------
+
+Finds the phmap library.
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+This module provides the following imported targets, if found:
+
+``parallel_hashmap``
+  The parallel_hashmap library
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``PHMAP_FOUND``
+  True if the system has the phmap library.
+``PHMAP_VERSION``
+  The version of the phmap library which was found.
+``PHMAP_INCLUDE_DIRS``
+  Include directories needed to use phmap.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``PHMAP_INCLUDE_DIR``
+  The directory containing ``parallel_hashmap/phmap_config.h``.
+
+#]=======================================================================]
+find_package(PkgConfig)
+pkg_check_modules(PC_phmap QUIET phmap)
+
+find_path(PHMAP_INCLUDE_DIR
+    NAMES parallel_hashmap/phmap_config.h
+    PATHS
+        ${PC_PHMAP_INCLUDE_DIRS}
+        ${CMAKE_INSTALL_PREFIX}/include
+        ${PROJECT_SOURCE_DIR}/third_party
+    PATH_SUFFIXES 
+        parallel_hashmap
+)
+
+macro(_phmap_get_version)
+    file(READ "${PHMAP_INCLUDE_DIR}/parallel_hashmap/phmap_config.h" _header)
+    string(CONCAT VERSION_REGEX
+        "#define[ \t]+PHMAP_VERSION_MAJOR[ \t]+([0-9]+)[ \n]+"
+        "#define[ \t]+PHMAP_VERSION_MINOR[ \t]+([0-9]+)[ \n]+"
+        "#define[ \t]+PHMAP_VERSION_PATCH[ \t]+([0-9]+)")
+    string(REGEX MATCH ${VERSION_REGEX} _ "${_header}")
+    # Use math to skip leading zeros if any.
+    math(EXPR PHMAP_VERSION_MAJOR ${CMAKE_MATCH_1})
+    math(EXPR PHMAP_VERSION_MINOR ${CMAKE_MATCH_2})
+    math(EXPR PHMAP_VERSION_PATCH ${CMAKE_MATCH_3})
+    string(JOIN "." PHMAP_VERSION ${PHMAP_VERSION_MAJOR}
+                                  ${PHMAP_VERSION_MINOR}
+                                  ${PHMAP_VERSION_PATCH})
+endmacro()
+
+set(PHMAP_VERSION ${PC_PHMAP_VERSION})
+if (NOT PHMAP_VERSION)
+    _phmap_get_version()
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(phmap
+    FOUND_VAR PHMAP_FOUND
+    REQUIRED_VARS
+        PHMAP_INCLUDE_DIR
+    VERSION_VAR PHMAP_VERSION
+)
+
+if(PHMAP_FOUND)
+    set(PHMAP_INCLUDE_DIRS ${PHMAP_INCLUDE_DIR})
+    set(PHMAP_DEFINITIONS ${PC_PHMAP_CFLAGS_OTHER})
+endif()
+
+if(PHMAP_FOUND AND NOT TARGET phmap)
+    add_library(phmap INTERFACE IMPORTED)
+    set_target_properties(phmap PROPERTIES
+        INTERFACE_COMPILE_OPTIONS "${PC_PHMAP_CFLAGS_OTHER}"
+        INTERFACE_INCLUDE_DIRECTORIES "${PHMAP_INCLUDE_DIR}"
+    )
+endif()
+
+mark_as_advanced(PHMAP_INCLUDE_DIR)
