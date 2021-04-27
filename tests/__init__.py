0764aa5196d9a78def8eb948c7d17ca1c0f6be6c
Bruno Schmitt <bruno.schmitt@epfl.ch>
2021-04-27 09:46:01 +0200

    Remove unecessary __init__ file

--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-#-------------------------------------------------------------------------------
-# Part of Tweedledum Project.  This file is distributed under the MIT License.
-# See accompanying file /LICENSE for details.
-#-------------------------------------------------------------------------------
-"""Tweedledum unit tests."""
