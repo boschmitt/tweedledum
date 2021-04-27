f8a7aff6ba9876a00245a11ea27113688337834c
Bruno Schmitt <bruno.schmitt@epfl.ch>
2021-04-27 16:28:51 +0200

    A gambiarra to make develop mode install work

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import os
 import sys
 import setuptools
 import time
+from pathlib import Path
 
 build_dev_version = os.environ.get('TWEEDLEDUM_DEV_VERSION', False)
 
@@ -46,7 +47,7 @@ skbuild.setup(
     author_email='bruno.schmitt@epfl.ch',
     license="MIT",
     package_dir={'': 'python'},
-    packages=setuptools.find_packages(where='python'),
+    packages=setuptools.find_packages(where='python', include=['tweedledum', 'tweedledum.*']),
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.6",
@@ -63,5 +64,21 @@ skbuild.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9"
-    ]
+    ],
+    cmake_install_dir='python'
 )
+
+# /!\ This is a 'gambiarra':
+# For whatever reason, whenever I try to install `tweedledum` in development 
+# mode, i.e., `pip install -e .`, the easy_path and egg-link are wrong! Hence, 
+# I basically fix them by hand because I couldn't figure out a better way of 
+# doing it!
+if len(sys.argv) > 1 and sys.argv[1] == "develop":
+    import fileinput
+    import shutil
+    import site
+    easy_install = Path(site.getsitepackages()[0]) / 'easy-install.pth'
+    egg_link = Path(site.getsitepackages()[0])  / 'tweedledum.egg-link'
+    for line in fileinput.input([easy_install, egg_link], inplace=True):
+        print(line.replace("tweedledum", "tweedledum/python"),  end="")
+    shutil.rmtree('tweedledum.egg-info', ignore_errors=True)
