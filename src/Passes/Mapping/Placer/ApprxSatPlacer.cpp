5d58a889ff88352394f71a8fbdd50dccc5086afe
Bruno Schmitt <bruno.schmitt@epfl.ch>
2021-05-06 18:59:18 +0200

    Add approximate heristic.

--- src/Passes/Mapping/Placer/ApprxSatPlacer.cpp
+++ src/Passes/Mapping/Placer/ApprxSatPlacer.cpp
@@ -8,11 +8,12 @@ namespace tweedledum {
 
 /*! \brief Yet to be written.
  */
-std::optional<Placement> apprx_sat_place(Device const& device, 
-    Circuit const& original)
+std::optional<Placement> apprx_sat_place(Device const& device,
+    Circuit const& original, nlohmann::json const& config)
 {
+    bool use_weight = true;
     bill::solver solver;
-    ApprxSatPlacer placer(device, original, solver);
+    ApprxSatPlacer placer(device, original, solver, use_weight);
     return placer.run();
 }
 
