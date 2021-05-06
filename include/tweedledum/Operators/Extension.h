18f1df66044d9b148fe0cc767c1c410c93484bf7
Bruno Schmitt <bruno.schmitt@epfl.ch>
2021-05-06 14:27:54 +0200

    Add Bridge operator.
    This operator only makes sense when dealing with mapped circuits.
    A `Bridge` operator is basically a fancy `X`.  Functionally they behave
    in the exact same way.  However, Bridge does not requires its qubits to
    be connected, because its decomposition should take into account the
    device coupling constraints.

--- include/tweedledum/Operators/Extension.h
+++ include/tweedledum/Operators/Extension.h
@@ -4,6 +4,7 @@
 *-----------------------------------------------------------------------------*/
 #pragma once
 
+#include "Extension/Bridge.h"
 #include "Extension/LogicNetwork.h"
 #include "Extension/Parity.h"
 #include "Extension/Permutation.h"
