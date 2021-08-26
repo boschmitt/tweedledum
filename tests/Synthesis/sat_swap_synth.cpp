f07c11e4696124a2b1afc49862c631efb4f28f13
Bruno Schmitt <bruno.schmitt@epfl.ch>
2021-08-26 23:33:19 +0200

    Add more tests

--- tests/Synthesis/sat_swap_synth.cpp
+++ tests/Synthesis/sat_swap_synth.cpp
@@ -29,3 +29,17 @@ TEST_CASE("Synthesis of swapping networks using SAT", "[sat_swap_synth][synth]")
         CHECK(check_unitary(expected, synthesized));
     }
 }
+
+TEST_CASE("Synthesis of swapping networks using SAT2", "[sat_swap_synth][synth]")
+{
+    Device device = Device::path(5u);
+
+    nlohmann::json config;
+    config["sat_swap_synth"]["opt_goal"] = "depth";
+    std::vector<uint32_t> init_cfg = {0, 1, 2, 3, 4};
+    std::vector<uint32_t> final_cfg = {0, 2, 4, 3, 1};
+    Circuit synthesized_swaps = sat_swap_synth(device, init_cfg, final_cfg);
+    Circuit synthesized_depth =
+      sat_swap_synth(device, init_cfg, final_cfg, config);
+    CHECK(check_unitary(synthesized_swaps, synthesized_depth));
+}
